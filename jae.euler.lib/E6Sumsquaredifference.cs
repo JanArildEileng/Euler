5cb3c387d01d2391aee4c2fb0f75591bf4aa7d9b
janarild.eileng <janarild.eileng@fhi.no>
2019-03-18 14:50:56 +0100

    E6Sumsquaredifference

---
+++ jae.euler.lib/E6Sumsquaredifference.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E6Sumsquaredifference
+    {
+        public long DifferenceBetweenTheSumOfSquares(long number)
+        {
+            var sum1 = SumOffthesquares(number);
+            var sum2 = SqueareOfSum(number);
+
+            return (sum2 - sum1);
+        }
+
+        private long SumOffthesquares(long number)
+        {
+            long total=0;
+            for(long i=1;i<=number;i++)
+            {
+                total += i * i;
+            }
+
+            return total;
+        }
+
+
+        private long SqueareOfSum(long number)
+        {
+            long sum = 0;
+            for (long i = 1; i <= number; i++)
+            {
+                sum += i ;
+            }
+
+            return sum*sum;
+        }
+
+
+    }
+}
---
+++ jae.euler.test/E6SumsquaredifferenceUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using jae.euler.lib;
+using System;
+using Xunit;
+
+namespace jae.euler.test
+{
+    public class E6SumsquaredifferenceUnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            /*
+            
+           Sum of the squares of the first ten natural numbers is,  
+           12 + 22 + ... + 102 = 385
+           The square of the sum of the first ten natural numbers is,
+           (1 + 2 + ... + 10)2 = 552 = 3025
+            Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+             
+             */
+
+            var sut = new E6Sumsquaredifference();
+
+            var diff = sut.DifferenceBetweenTheSumOfSquares(10);
+            Assert.Equal(2640, diff);
+
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+            
+           Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+            */
+
+
+            var sut = new E6Sumsquaredifference();
+
+
+            var diff = sut.DifferenceBetweenTheSumOfSquares(100);
+            Assert.Equal(25164150, diff);
+            /*
+                    Congratulations, the answer you gave to problem 6 is correct.
+
+                    You are the 431615th person to have solved this problem.
+            */
+
+        }
+    }
+}
