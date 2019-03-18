79ec68db8d13a041959e1628b6c5fd8d0caa3e44
janarild.eileng <janarild.eileng@fhi.no>
2019-03-18 09:13:15 +0100

    E3Largestprimefactor

---
+++ jae.euler.lib/E3Largestprimefactor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using jae.euler.lib.Primes;
+
+namespace jae.euler.lib
+{
+    public class E3Largestprimefactor
+    {
+        public long  Largest(long below)
+        {
+
+            List<long> primeList = PrimeList.GetList(below);
+
+            return primeList.Max();
+
+
+        }
+
+
+    }
+}
