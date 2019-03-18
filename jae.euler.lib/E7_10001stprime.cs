2345129d01013abf75993e87d9b3e98e134a93f3
janarild.eileng <janarild.eileng@fhi.no>
2019-03-18 15:10:36 +0100

    E7_10001stprimeUnitTest

---
+++ jae.euler.lib/E7_10001stprime.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E7_10001stprime
+    {
+        public object GetPrimeNumber(long primenr)
+        {
+            var listPrimeNumber = new List<long>();
+
+            long prime = 2;
+
+            listPrimeNumber.Add(prime);
+
+            while (listPrimeNumber.Count < primenr)
+            {
+                prime = GetNextPrime(prime, listPrimeNumber);
+                listPrimeNumber.Add(prime);
+            }
+
+
+            return prime;
+        }
+
+        private long GetNextPrime(long prime, List<long> listPrimeNumber)
+        {
+            var next = prime;
+
+            while ( !IsPrime(next, listPrimeNumber))
+            {
+                next++;
+            }
+            return next;
+        }
+
+        private bool IsPrime(long next, List<long> listPrimeNumber)
+        {
+            for(int i=0;i< listPrimeNumber.Count;i++)
+            {
+                if (next % listPrimeNumber[i] == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ jae.euler.test/E7_10001stprimeUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using jae.euler.lib;
+using System;
+using Xunit;
+
+namespace jae.euler.test
+{
+    public class E7_10001stprimeUnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            /*
+              By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.            
+           */
+
+            var sut = new E7_10001stprime();
+
+            long expected = 13;
+            var value = sut.GetPrimeNumber(6);
+            Assert.Equal(expected, value);
+
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+            
+                What is the 10 001st prime number?
+       */
+
+
+            var sut = new E7_10001stprime();
+
+            long expected = 104743;
+            var value = sut.GetPrimeNumber(10001);
+            Assert.Equal(expected, value);
+
+            /*
+                    Congratulations, the answer you gave to problem 7 is correct.
+
+                        You are the 368949th person to have solved this problem.
+            */
+
+        }
+    }
+}
