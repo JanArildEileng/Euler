b470a6b582518649158351461c1f34056c8d377c
janarild.eileng <janarild.eileng@fhi.no>
2019-03-18 15:52:52 +0100

    E8Largestproductinaseries

---
+++ jae.euler.lib/E8Largestproductinaseries.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E8Largestproductinaseries
+    {
+        public long GetLagrestProduct(long nrSeq, string testString)
+        {
+            // var ToCharArray = testString.ToCharArray().Select(c=> Convert.ToInt32(c)).ToArray();
+
+            long[] ToCharArray = new long[1000];
+
+            for(int i=0;i< testString.Length;i++)
+            {
+                ToCharArray[i] = int.Parse(testString.Substring(i, 1));
+            }
+          
+            
+
+
+
+            long slutt = ToCharArray.Length - nrSeq + 1;
+            long maxProduct = 0;
+
+            for(int i=0; i< slutt;i++)
+            {
+                long product = 1;
+                for(var j=0;j< nrSeq;j++)
+                {
+                    product *= ToCharArray[j+i];
+                }
+                if (product > maxProduct) maxProduct = product;
+
+            }
+
+
+
+            return maxProduct;
+        }
+    }
+}
