aaa5e32e574ba7840e1b3ed2f236f524f85a94a3
janarild.eileng <janarild.eileng@fhi.no>
2019-03-20 14:42:13 +0100

    E012Highlydivisibletriangularnumber Unkomplett

---
+++ jae.euler.lib/E012Highlydivisibletriangularnumber.cs
@@ -0,0 +1,94 @@
+﻿using jae.euler.math;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E012Highlydivisibletriangularnumber
+    {
+        public long GetFirstWithOver(long divisors)
+        {
+            var t = new TriangularNumber();
+
+
+            foreach (var tn in t.Sequence())
+            {
+                List<long> PrimeFactorsInNumber = Primes.GetPrimeFactorsInNumber(tn).ToList();
+                List<long> ekstar = new List<long>() { 1, tn };
+                ekstar.AddRange(PrimeFactorsInNumber);
+
+
+                int max = PrimeFactorsInNumber.Count;
+
+
+                for (int i=0;i<max-1;i++)
+                {
+                    long prod = PrimeFactorsInNumber[i];
+                    for(int j=i+1;j<max;j++)
+                    {
+                        prod = prod * PrimeFactorsInNumber[j];
+                        ekstar.Add(prod);
+                    }
+
+                    
+                }
+
+
+                var count =ekstar.Distinct().ToList().Count;
+
+                if (count > divisors) return tn;
+
+
+            }
+
+
+            return 1;
+        }
+
+
+
+        private long getNumberOfDivisors(long number)
+        {
+            long numberOfDivisors = 1;
+            if (number == 1) return numberOfDivisors;
+            //seg selv og 1
+            numberOfDivisors = 2;
+
+
+
+
+
+            return numberOfDivisors;
+        }
+
+
+
+
+
+    }
+
+    public class TriangularNumber
+    {
+
+        public IEnumerable<long> Sequence()
+        {
+            long n = 1;
+            long sum = 0;
+
+
+            while (true )
+            {
+                sum += n ;
+                n++;
+                yield return sum;
+            }
+
+        }
+
+
+    }
+
+
+}
---
+++ jae.euler.runner/Program.cs
@@ -0,0 +1,64 @@
+﻿using jae.euler.math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace jae.euler.runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int minshow = 0;
+
+            var t = new TriangularNumber();
+
+            int n = 0;
+            foreach (var tn in t.Sequence())
+            {
+                List<long> PrimeFactorsInNumber = Primes.GetPrimeFactorsInNumber(tn).ToList();
+
+                n++;
+
+                if (PrimeFactorsInNumber.Count <= minshow) continue;
+
+            //    PrimeFactorsInNumber.Insert(0, 1);
+                Console.Write($"{n,3}  {tn,10} :");
+
+                PrimeFactorsInNumber.Reverse();
+
+                foreach (var a in PrimeFactorsInNumber)
+                    Console.Write($"{a,4},");
+                Console.WriteLine("");
+
+                minshow = PrimeFactorsInNumber.Count;
+
+                if (tn > 1000000000) break;
+            }
+
+        }
+    }
+
+
+    public class TriangularNumber
+    {
+
+        public IEnumerable<long> Sequence()
+        {
+            long n = 1;
+            long sum = 0;
+
+
+            while (true)
+            {
+                sum += n;
+                n++;
+                yield return sum;
+            }
+
+        }
+
+
+    }
+}
