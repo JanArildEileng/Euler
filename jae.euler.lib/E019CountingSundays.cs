238c5d3b3ebddb44fc83cec649d72acab836bfcd
janarild.eileng <janarild.eileng@fhi.no>
2019-03-27 12:25:20 +0100

    E019CountingSundays

---
+++ jae.euler.lib/E019CountingSundays.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E019CountingSundays
+    {
+        public int CountingSundays(DateTime fromDate, DateTime toDate)
+        {
+            int sundays = 0;
+            int []thirtydaysmonth = new int[]{ 4, 6, 9, 11 };
+      
+
+            var date1901_1_01 = new DateTime(1901, 1, 1);
+            int DaysFrom_1900_1_01 = CalcDaysFrom_1900_1_01(date1901_1_01);
+            int dayOfWeek_1900_1_01 = DaysFrom_1900_1_01 % 7;  // 0=monday; 6 ==sunday
+
+
+             DateTime currentDate = fromDate;
+
+            int daysAdded = 0;
+
+            int year = currentDate.Year;
+            int month = currentDate.Month;
+            int day = currentDate.Day;
+
+            while (year <= toDate.Year && month<= toDate.Month && day<= toDate.Day)
+            {
+                //sjekk om ny mnd
+                if (day == 31 || (day == 30 && thirtydaysmonth.Contains(month) )) { day = 0; month++; }
+
+                if (month == 2 && ( day == 29 || (day == 28 && year % 4 != 0) || (day == 28 && (year % 100 == 0 && year % 400 != 0)))) { day = 0; month++; }
+
+                
+                //sjekk om neste år
+                if ( month > 12 ) { month = 1; year++; }
+
+                //sjekk om sønday er på 1st
+                if (day==1)
+                {
+                   if (  (DaysFrom_1900_1_01+ daysAdded) % 7==6)
+                    {
+                        sundays++;
+                    }
+                }
+
+                //neste dag
+
+                daysAdded++;
+                day++;
+            }
+
+            return sundays;
+        }
+
+
+        public int CalcDaysFrom_1900_1_01(DateTime currentDate)
+        {
+            var fromDate = new DateTime(1900, 1, 1);
+
+            int daysAdded = 0;
+
+            while (currentDate> fromDate)
+            {
+                fromDate=fromDate.AddDays(1);
+                daysAdded++;
+            }
+
+            return daysAdded;
+        }
+
+       
+
+    }
+}
---
+++ jae.euler.test/E019CountingSundaysUnitTest.cs
@@ -0,0 +1,90 @@
+﻿using jae.euler.lib;
+using System;
+using Xunit;
+using System.Linq;
+using System.Text;
+
+namespace jae.euler.test
+{
+    public class E019CountingSundaysUnitTest
+    {
+        [Fact]
+        public void TestBaselineDates()
+        {
+            /*
+             * 1 Jan 1900 was a Monday.
+            */
+             
+            var fromDate = new DateTime(1900, 1, 1);
+
+            var dayOfWeek = fromDate.DayOfWeek;
+            Assert.Equal(DayOfWeek.Monday,dayOfWeek);
+
+            int sundays = 0;
+            for(int i = 1; i <= 12; i++)
+            {
+                var firstOfMonth = new DateTime(1900, i, 1);
+                if (firstOfMonth.DayOfWeek == DayOfWeek.Sunday)
+                    sundays++;
+            }
+
+            //2 sundays in 1900
+            Assert.Equal(2, sundays);
+
+
+            sundays = 0;
+            for(int year=1901;year<= 2000; year++)
+            for (int i = 1; i <= 12; i++)
+            {
+                var firstOfMonth = new DateTime(year, i, 1);
+                if (firstOfMonth.DayOfWeek == DayOfWeek.Sunday)
+                    sundays++;
+
+            }
+            //171 sundays in 1901 -2000
+            Assert.Equal(171, sundays);
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+            /*
+                How many Sundays fell on the first of the month    1901/1902       
+            */
+            var sut = new E019CountingSundays();
+            var fromDate = new DateTime(1901, 1, 1);
+            var toDate = new DateTime(1902, 12, 31);
+            int sundaysExpected = 3;
+            int sundays = sut.CountingSundays(fromDate: fromDate, toDate: toDate);
+            Assert.Equal(sundaysExpected, sundays);
+
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+                How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+            */
+
+            var sut = new E019CountingSundays();
+
+            var fromDate = new DateTime(1901, 1, 1);
+            var toDate = new DateTime(2000, 12, 31);
+
+            int sundaysExpected = 171;
+            int sundays = sut.CountingSundays(fromDate: fromDate, toDate: toDate);
+            Assert.Equal(sundaysExpected, sundays);
+
+            /*
+              Congratulations, the answer you gave to problem 19 is correct.
+
+                You are the 119283rd person to have solved this problem.
+            */
+
+        }
+
+
+    }
+}
