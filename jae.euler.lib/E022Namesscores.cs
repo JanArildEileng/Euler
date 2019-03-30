d816a8cb47d161ce41552236393ad07a1471dd47
janarild.eileng <janarild.eileng@fhi.no>
2019-03-30 09:41:10 +0100

    E022Namesscores

---
+++ jae.euler.lib/E022Namesscores.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using jae.euler.lib.Extender;
+
+namespace jae.euler.lib
+{
+    public class E022Namesscores
+    {
+        /*
+
+           begin by sorting it into alphabetical order. 
+            Then working out the alphabetical value for each name, 
+            multiply this value by its alphabetical position in the list to obtain a name score.
+         */
+
+        public long GetNamesscores(List<string> nameList)
+        {
+            long namesscores = 0;
+    
+            int alphabeticatposition = 1;
+
+            foreach (string name in nameList.OrderBy(e => e).ToList())
+            {
+                 namesscores += alphabeticatposition * name.AlphabeticalValue(); 
+                 alphabeticatposition++;     
+            }
+
+            return namesscores;
+        }
+    }
+}
---
+++ jae.euler.lib/Extender/StringExtender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib.Extender
+{
+    public static class StringExtender
+    {
+        public static long AlphabeticalValue(this string str) 
+        {
+            List<int> sumNumbers = str.ToUpper().ToCharArray().Select(c => Convert.ToInt32(c - 'A') + 1).ToList();
+
+            return sumNumbers.Sum();
+        }
+      
+
+    }
+}
