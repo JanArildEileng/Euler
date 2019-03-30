80a4c1cb301f21d85935032cbf1b2047ec89d5b3
jan eileng <jaeileng@gmail.com>
2019-03-30 17:46:04 +0100

    E026Reciprocalcycles

---
+++ jae.euler.lib/E026Reciprocalcycles.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E026Reciprocalcycles
+    {
+        struct divrest
+        {
+            public int div;
+            public int rest;
+        }
+
+
+        public int Getlongestrecurringcycledenominator(int maxdenominator)
+        {
+            int longestrecurringcycle = 0;
+            int longestrecurringcycledenominator = 1;
+            for (var denominator=2; denominator<= maxdenominator; denominator++)
+            {
+                int recurringcycle = Getrecurringcycle(denominator);
+                if (recurringcycle > longestrecurringcycle)
+                {
+                    longestrecurringcycle = recurringcycle;
+                    longestrecurringcycledenominator = denominator;
+                }
+            }
+            return longestrecurringcycledenominator;
+        }
+
+        private int Getrecurringcycle(int denominator)
+        {
+            List<divrest> cycleListe = new List<divrest>();
+
+            int cycle = 1;
+            int numerator = 10;
+
+            int div = numerator / denominator;
+            int rest = numerator % denominator;
+            cycleListe.Add(new divrest { div = div, rest = rest });
+            
+
+           
+                
+            while (rest > 0 ) {
+                rest = rest * 10;
+                div = rest / denominator;
+                rest = rest % denominator;
+              
+
+                if ( cycleListe.Where(e=>e.div==div && e.rest==rest).Count() >0)
+                {
+                    return cycleListe.Count();
+                } else
+                {
+                    cycleListe.Add(new divrest { div = div, rest = rest });
+                }
+               
+               
+            }
+
+
+                    
+            return cycleListe.Count();
+        }
+    }
+}
---
+++ jae.euler.test/E026ReciprocalcyclesUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+
+namespace jae.euler.test
+{
+    public class E026ReciprocalcyclesUnitTest
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            /*
+           It can be seen that 1/7 has a 6-digit recurring cycle.
+            */
+
+            var sut = new E026Reciprocalcycles();
+            int denominator = sut.Getlongestrecurringcycledenominator(maxdenominator: 10);
+            Assert.Equal(7, denominator);
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+            Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+             */
+
+            var sut = new E026Reciprocalcycles();
+            int denominator = sut.Getlongestrecurringcycledenominator(maxdenominator: 1000);
+            Assert.Equal(983, denominator);
+
+
+            /*
+               Congratulations, the answer you gave to problem 25 is correct.
+
+                You are the 137634th person to have solved this problem.
+                Nice work, janei, you've just advanced to Level 1 . 
+                106777 members (11.92%) have made it this far.
+            */
+        }
+    }
+}
