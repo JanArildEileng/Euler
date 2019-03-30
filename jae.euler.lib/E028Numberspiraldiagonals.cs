caee8e5cf89ae7ad90c4598de66d93fa2671edbd
janarild.eileng <janarild.eileng@fhi.no>
2019-03-30 20:34:39 +0100

    E028Numberspiraldiagonals

---
+++ jae.euler.lib/E028Numberspiraldiagonals.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E028Numberspiraldiagonals
+    {
+        public int GetSumDiagonal(int size)
+        {
+            int sumDiagonals = 1;
+            int counter = 1;
+            int stepsize = 2;
+
+
+            while (counter <size*size)
+            {
+                for(int i=0;i<4;i++)
+                {
+                    counter += stepsize;
+                    sumDiagonals += counter;
+                }
+                stepsize += 2;
+            }
+
+            return sumDiagonals;
+
+        }
+    }
+}
---
+++ jae.euler.test/E028NumberspiraldiagonalsUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+
+namespace jae.euler.test
+{
+    public class E028NumberspiraldiagonalsUnitTest
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            /*
+             Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+             It can be verified that the sum of the numbers on the diagonals is 101.
+            */
+
+            var sut = new E028Numberspiraldiagonals();
+
+            Assert.Equal(101, sut.GetSumDiagonal(size:5));          
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+                 What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+             */
+
+            var sut = new E028Numberspiraldiagonals();
+
+            Assert.Equal(669171001, sut.GetSumDiagonal(size: 1001));
+
+            /*
+                Congratulations, the answer you gave to problem 28 is correct.
+                You are the 96428th person to have solved this problem.
+            */
+        }
+    }
+}
