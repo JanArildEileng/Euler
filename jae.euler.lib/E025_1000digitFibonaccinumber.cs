88ecb86253ace004f554c2cdab8070300c0c9701
janarild.eileng <janarild.eileng@fhi.no>
2019-03-30 11:11:52 +0100

    E025_1000digitFibonaccinumber

---
+++ jae.euler.lib/E025_1000digitFibonaccinumber.cs
@@ -0,0 +1,29 @@
+﻿using jae.euler.lib.Digits;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E025_1000digitFibonaccinumber
+    {
+        public long GetIndexOfFirstTerm(int numberOfdigits)
+        {
+
+            List<int> fn1 = new List<int>() { 1 };
+            List<int> fn2 = new List<int>() { 1 };
+   
+            int IndexOfTerm = 2;
+
+            while (fn2.Count < numberOfdigits)
+            {
+                var fnext = DigitsList.Sum(fn1, fn2);
+                fn1 = fn2;
+                fn2 = fnext;
+                IndexOfTerm++;
+            }
+
+            return IndexOfTerm;
+        }
+    }
+}
