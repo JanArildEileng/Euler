77c0a5c4fb19104068ac92164696f41e81c7a461
janarild.eileng <janarild.eileng@fhi.no>
2019-04-01 16:45:32 +0200

    E033Digitcancellingfractions wip

---
+++ jae.euler.lib/E033Digitcancellingfractions.cs
@@ -0,0 +1,97 @@
+﻿using jae.euler.util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E033Digitcancellingfractions
+    {
+
+        public int GeLowestCommonTerms()
+        {
+            for(int denominator=10; denominator<100; denominator++)
+            {
+
+                for (int numerator = 10; numerator < denominator; numerator++)
+                {
+                    int ahigh = numerator /10;
+                    int alow = numerator % 10;
+
+                    int bhigh = denominator / 10;
+                    int blow = denominator % 10;
+
+                    if (denominator==98 && numerator==49)
+                    {
+                        var t2 = 1;
+                    }
+
+
+                    if (blow == 0 || alow == 0) continue;
+            
+                    if (ahigh ==blow )
+                    {
+                        ahigh = blow = 0;
+                    } else 
+
+                    if (alow == bhigh)
+                    {
+                        alow = bhigh = 0;
+                    }
+
+                    int a = ahigh * 10 + alow;
+                    int b = bhigh * 10 + blow;
+
+
+                    if ( a!= numerator || b!= denominator)
+                    {
+                        if (denominator % b==0)
+                        {
+                            int div = denominator / b;
+                            if ( a* div== numerator)
+                            {
+                                var t = 1;
+                            }
+
+                        }
+              
+                       
+                    }
+
+
+
+                }
+
+
+            }
+
+
+            return 1;
+        }
+
+
+
+
+        public bool IsDigitcancellingfractions(int numerator, int denominator)
+        {
+
+            List<int> numeratorDigits = DigitsList.ConvertToDigitListe(numerator);
+            List<int> denominatorDigits = DigitsList.ConvertToDigitListe(denominator);
+
+
+
+
+            throw new NotImplementedException();
+        }
+
+
+        public int LowestCommonTerms(int a, int b)
+        {
+
+
+            return 1;
+        }
+
+       
+    }
+}
---
+++ jae.euler.test/E033DigitcancellingfractionsUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+
+namespace jae.euler.test
+{
+    public class E033DigitcancellingfractionsUnitTes
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            /*
+              The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting to simplify it may incorrectly believe that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+              We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+              There are exactly four non-trivial examples of this type of fraction, 
+              less than one in value, and containing two digits in the numerator and denominator.
+
+            */
+
+            var sut = new E033Digitcancellingfractions();
+
+
+            Assert.Equal(1,sut.LowestCommonTerms(2,3));
+            Assert.Equal(6, sut.LowestCommonTerms(2, 6));
+            Assert.Equal(18, sut.LowestCommonTerms(6, 9));
+
+
+
+
+            Assert.True(sut.IsDigitcancellingfractions(numerator: 49, denominator: 98));
+            Assert.False(sut.IsDigitcancellingfractions(numerator: 48, denominator: 84));
+
+
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+              If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
+
+             */
+
+            var sut = new E033Digitcancellingfractions();
+            var result = sut.GeLowestCommonTerms();
+            Assert.Equal(1, result);
+            /*
+              Congratulations, the answer you gave to problem 32 is correct.
+              you are the 62497th person to have solved this problem.
+            */
+        }
+    }
+
+   
+}
