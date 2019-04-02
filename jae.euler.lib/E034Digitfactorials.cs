0f39d0a69c04118a57b7f687644bc41f666874f5
janarild.eileng <janarild.eileng@fhi.no>
2019-04-02 16:43:31 +0200

    E034Digitfactorials

---
+++ jae.euler.lib/E034Digitfactorials.cs
@@ -0,0 +1,35 @@
+﻿using jae.euler.math;
+using jae.euler.util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E034Digitfactorials
+    {
+        private long[] FactorialArray;
+
+        public E034Digitfactorials()
+        {
+            //pre calc n! for n=0 til 9 ..
+            FactorialArray = Enumerable.Range(0, 10).Select(digit => Factorial.Factor(digit)).ToArray();
+        }
+
+        public long GetSumOfAllCuriousNumbers()
+        {      return Enumerable.Range(3, 2540161).Where(i => IsCuriousNumber(i)).Sum();
+        }
+
+        /*
+               145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+        */
+        public bool IsCuriousNumber(int number)
+        {
+            return number == DigitsList
+                .ConvertToDigitListe(number)
+                .Select(digit => FactorialArray[digit])
+                .Sum();
+        }
+    }
+}
---
+++ jae.euler.math/Factorial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.math
+{
+    public class Factorial
+    {
+        public static long Factor(long number)
+        {
+            if (number < 2) return 1;
+            return number * Factor(number - 1);
+        }
+    }
+}
