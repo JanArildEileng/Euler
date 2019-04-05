c38b7f5d1da2ef1de37c1cde31d9a0a1949a304b
janarild.eileng <janarild.eileng@fhi.no>
2019-04-05 08:25:33 +0200

    E037Truncatableprimes

---
+++ jae.euler.lib/E037Truncatableprimes.cs
@@ -0,0 +1,49 @@
+﻿using jae.euler.math;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using jae.euler.util;
+
+namespace jae.euler.lib
+{
+    public class E037Truncatableprimes
+    {
+        Dictionary<long, long> primemap;
+
+        public E037Truncatableprimes(int max)
+        {
+            primemap = Primes.GetPrimeFactorsBelowNumber(max).ToDictionary(e => e);
+        }
+
+        public long GetSumOfTruncatableprime(int numberof)
+        {
+           return primemap
+                .Values
+                .Where(e => e > 7)
+                .Where(e => IsTruncatableprime((int)e))
+                .Sum();
+        }
+
+        public bool IsTruncatableprime(int number)
+        {
+            var digits = DigitsList.ConvertToDigitListe(number);
+
+            for(int i=0;i< digits.Count;i++)
+                if (!IsPrime(DigitsList.ConvertToNumber(digits.Skip(i).ToList()))) return false;
+
+            for (int i = 1; i < digits.Count; i++)
+                if (!IsPrime(DigitsList.ConvertToNumber(digits.SkipLast(i).ToList()))) return false;
+
+            return true;
+        }
+
+
+        private bool IsPrime(int number)
+        {
+            if (primemap.ContainsKey(number)) return true;
+            return false;
+        }
+      
+    }
+}
---
+++ jae.euler.test/E037TruncatableprimesUnitTest .cs
@@ -0,0 +1,45 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+
+namespace jae.euler.test
+{
+    public class E037TruncatableprimesUnitTest
+    {
+     
+        [Fact]
+        public void Test1()
+        {
+            /*
+                 The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. 
+                 Similarly we can work from right to left: 3797, 379, 37, and 3.      
+                 NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+            */
+
+            var sut = new E037Truncatableprimes(10000);
+            Assert.False(sut.IsTruncatableprime(419));
+            Assert.True(sut.IsTruncatableprime(3797));
+            Assert.False(sut.IsTruncatableprime(3795));
+            Assert.False(sut.IsTruncatableprime(4297));
+        }
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+              Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+            */
+
+            var sut = new E037Truncatableprimes(1000000);
+            Assert.Equal(748317, sut.GetSumOfTruncatableprime(numberof:11));
+
+            /*
+                Congratulations, the answer you gave to problem 37 is correct.
+                You are the 64873rd person to have solved this problem.
+            */
+        }
+    }
+
+    
+}
