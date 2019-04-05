209bbb0a4186acba0cb70df7b637a0ec95a73fbb
janarild.eileng <janarild.eileng@fhi.no>
2019-04-05 14:43:30 +0200

    E040Champernowneconstant

---
+++ jae.euler.lib/E040Champernowneconstant.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E040Champernowneconstant
+    {
+
+        char[] Champernowneconstant;
+
+        public E040Champernowneconstant(int maxDigits)
+        {
+
+            Champernowneconstant = CalcChampernowneconstant(maxDigits);
+        }
+
+
+        public int GetDigitNumber(int number)
+        {
+            return  Champernowneconstant[number]-'0';
+        }
+
+
+        private char[] CalcChampernowneconstant(int maxDigits)
+        {
+
+            StringBuilder builder = new StringBuilder();
+            int totalStrLen = 0;
+
+            //to algin array with number index
+            builder.Append(".");
+
+            for (int i=1; totalStrLen <= maxDigits;i++)
+            {
+                var str = i.ToString();
+                totalStrLen += str.Length;
+                builder.Append(str);
+            }
+
+            return builder.ToString().ToCharArray();
+        }
+
+
+
+    //    d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+        public int  GetDigitNumberProduct()
+        {
+            int product = 1;
+            for (int i = 1; i <= 1000000; i *= 10)
+            {
+                product *= GetDigitNumber(i);
+            }
+
+            return product;
+        }
+    }
+}
