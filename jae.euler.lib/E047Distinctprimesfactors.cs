58ede1931846aadcf265b9c2908ec0b0ce6a5772
janarild.eileng <janarild.eileng@fhi.no>
2019-04-08 19:46:30 +0200

    E047Distinctprimesfactors

---
+++ jae.euler.lib/E047Distinctprimesfactors.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using jae.euler.math;
+
+namespace jae.euler.lib
+{
+    public class E047Distinctprimesfactors
+    {
+        public long GetFirstConsecutivenumberWith(int primefactors,int numberConsecutive)
+        {
+            //TODO:ytelse...!
+            long number = 1;
+            while(true)
+            {
+
+                long distinctprimeFactorsInNumber = 0;
+                for (int j=0;j< numberConsecutive;j++)
+                {
+                    distinctprimeFactorsInNumber = Primes.GetPrimeFactorsInNumber(number+j).Distinct().Count();
+                  //  distinctprimeFactorsInNumber = GetDistinctprimeFactorsInNumber(number + j, primefactors);
+                    if (distinctprimeFactorsInNumber != primefactors)
+                    {
+                        number += j ;
+                        break;
+                    }
+                }
+
+                if (distinctprimeFactorsInNumber == primefactors)
+                    return number;
+
+
+
+                number++;
+
+
+
+            }
+
+
+
+
+        }
+
+
+        public  long GetDistinctprimeFactorsInNumber(long number,int maxprimefactors)
+        {
+           var rest = number;
+            var current = 2;
+            int primefactors = 0;
+            bool first = true;
+
+
+            while (current <= rest)
+            {
+                if (rest % current == 0)
+                {
+                        if (first)
+                    {
+                        primefactors++;
+                        first = false;
+                        if (primefactors > maxprimefactors) return -1;
+                    }
+
+                    rest = rest / current;
+                }
+                else
+                {
+                    first = true;
+                    current++;
+                }
+            }
+
+            return primefactors;
+        }
+    }
+}
---
+++ jae.euler.test/E047DistinctprimesfactorsUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+
+namespace jae.euler.test
+{
+    public class E047DistinctprimesfactorsUnitTest
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            /*
+            the first two consecutive numbers to have two distinct prime factors are:
+
+             14 = 2 × 7
+             15 = 3 × 5          
+             the first three consecutive numbers to have three distinct prime factors are:
+
+            644 = 2² × 7 × 23
+            645 = 3 × 5 × 43
+            646 = 2 × 17 × 19.
+
+            */
+            var sut = new E047Distinctprimesfactors();
+
+            Assert.Equal(14, sut.GetFirstConsecutivenumberWith(primefactors:2,numberConsecutive:2));
+            Assert.Equal(644, sut.GetFirstConsecutivenumberWith(primefactors: 3,numberConsecutive:3));
+
+        }
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+           Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?             
+             */
+
+            var sut = new E047Distinctprimesfactors();
+
+            Assert.Equal(134043, sut.GetFirstConsecutivenumberWith(primefactors: 4, numberConsecutive: 4));
+
+            /*
+                 Congratulations, the answer you gave to problem 47 is correct.
+
+                    You are the 50779th person to have solved this problem.
+             */
+        }
+    }
+
+
+}
