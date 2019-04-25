67b3ebab71d903158c424f05d3beefa39b5f5c47
janarild.eileng <janarild.eileng@fhi.no>
2019-04-25 15:17:48 +0200

    E056PowerfulDigitSum

---
+++ jae.euler.lib/E056PowerfulDigitSum.cs
@@ -0,0 +1,31 @@
+﻿using jae.euler.util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E056PowerfulDigitSum
+    {
+        public long GetMaximumDigitalSum(int abelow, int bbelow)
+        {
+            long maximumDigitalSum = 1;
+
+            for (int a = 1; a < abelow; a++)
+            {
+                List<int> aliste = DigitsList.ConvertToDigitListe(1);
+                for (int b = 1; b < bbelow; b++)
+                {
+                    aliste = DigitsList.Product(aliste, a);
+
+                    var digitalSum = aliste.Sum();
+                    if (digitalSum > maximumDigitalSum)
+                        maximumDigitalSum = digitalSum;
+
+                }
+            }
+            return maximumDigitalSum;
+        }
+    }
+}
---
+++ jae.euler.test/level03/E056PowerfulDigitSumUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+using System.Linq;
+
+namespace jae.euler.test.level03
+{
+    public class E056PowerfulDigitSumUnitTest
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            /*
+            */
+            var sut = new E056PowerfulDigitSum();
+        }
+
+   
+
+    
+
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+            Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?        
+           */
+
+            var sut = new E056PowerfulDigitSum();
+            Assert.Equal(972, sut.GetMaximumDigitalSum(abelow: 100,bbelow:100));
+
+
+
+            /*
+               Congratulations, the answer you gave to problem 56 is correct.
+
+                You are the 51494th person to have solved this problem.
+            */
+        }
+    }
+
+
+}
