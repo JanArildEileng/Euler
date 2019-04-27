680bfd1492c173be303af6a32e833c4fa4e39373
Jan Eileng <janarild.eileng@fhi.no>
2019-04-27 11:46:45 +0200

    E059XORDecryption

---
+++ jae.euler.lib/E059XORDecryption.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E059XORDecryption
+    {
+        public int GetSumAssicOfDekryptetText(List<int> cipherListe)
+        {
+            int start = (int)'a';
+            int slutt = (int)'z';
+
+           // the encryption key consists of three lower case characters
+           // lag alle kobinasjon inntil treff
+            for (int i1=start;i1<= slutt;i1++)
+            {
+                for (int i2= start; i2 <= slutt; i2++)
+                {
+                    for (int i3 = start; i3 <= slutt; i3++)
+                    {
+                        int key = (( (i1 << 8 ) +i2 ) <<8 ) + i3 ;
+                        var str=TryDekrypt(cipherListe, key);
+
+                        if (str!=null &&  str.Contains("the ") && str.Contains(" is ") )
+                        {
+                            var sum=str.ToCharArray().ToList().Sum(e => (int)e);
+                            return sum;
+                        }
+                    }
+                }
+            }
+
+            throw new Exception("Dekryptert tekst ikke funnet");
+        }
+
+        private string TryDekrypt(List<int> cipherListe, int key)
+        {
+
+            int len = cipherListe.Count;
+            char[] charbuffer = new char[len];
+            int insertIndex = 0;
+
+            int index =0;
+            while ( index< len) {
+                // slå sammen 3 tall , og dekrypter dette
+                int i1 = cipherListe[index++]<<16;
+                int i2 = cipherListe[index++]<<8;
+                int i3 = cipherListe[index++];
+              
+                int dekrypt = (i1 + i2 + i3) ^ key;
+
+                // hent ut 3 char fra dekryptert tall
+                int c1 = dekrypt >> 16;
+                int c2 = (dekrypt >> 8)  & 255;
+                int c3 = (dekrypt) & 255;
+
+                //hvis none-ascci , abort..ikke gyldig nøkkel
+                if (c1 > 127 || c2 > 127 || c3 > 127) return null;
+           
+                charbuffer[insertIndex++] = (char)c1;
+                charbuffer[insertIndex++] = (char)c2;
+                charbuffer[insertIndex++] = (char)c3;
+            }
+
+            return new string(charbuffer);
+
+        }
+
+
+
+
+
+
+        public char GetXor(char value, char key)
+        {
+            int b = (int)value;
+            int k = (int)key;
+            int xorresult = b ^ k;        
+            return (char)xorresult; ;
+        }
+    }
+}
---
+++ jae.euler.test/level03/E059XORDecryptionUnitTest.cs
@@ -0,0 +1,65 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace jae.euler.test.level03
+{
+    public class E059XORDecryptionUnitTest
+    {
+
+        private List<int> GetNamesFromFile()
+        {
+            /*
+                Using names.txt (right click and 'Save Link/Target As...'),
+                a 46K text file containing over five-thousand first names, 
+            */
+
+            var filecontents = File.ReadAllText("DataFiles\\p059_cipher.txt");
+            var charListe = filecontents.Split(new char[] { ',' });
+            var cipherListe = charListe.Select(e=> int.Parse(e)).ToList();
+            return cipherListe;
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+            /*
+                   For example, uppercase A = 65, asterisk (*) = 42, and lowercase k = 107.
+            */
+            var sut = new E059XORDecryption();
+
+            Assert.Equal('k', sut.GetXor('A', '*'));
+
+            List<int> cipherListe = GetNamesFromFile();
+        }
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+              find the sum of the ASCII values in the original text.
+            */
+
+            List<int> cipherListe = GetNamesFromFile();
+
+       
+
+            var sut = new E059XORDecryption();
+            Assert.Equal(129448, sut.GetSumAssicOfDekryptetText(cipherListe));
+
+
+            /*
+               Congratulations, the answer you gave to problem 59 is correct.
+
+                You are the 36531st person to have solved this problem.
+            */
+        }
+    }
+
+
+}
