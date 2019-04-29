ff8afaba9f46fa271e5f37d2355c94fb9536392c
janarild.eileng <janarild.eileng@fhi.no>
2019-04-29 08:48:07 +0200

    E062CubicPermutations

---
+++ jae.euler.lib/E062CubicPermutations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E062CubicPermutations
+    {
+        const int SIZE = 10000;
+        public long GetSmallestCube(int numberOfpermutations)
+        {
+            var cubes = Enumerable.Range(1, SIZE).
+                Select(n => (long)n * n * n).
+                Select(value => new  { value = value, permutations = ConvertToSmallestPermutationString(value) }).
+                ToList();
+
+            return cubes.
+                GroupBy(i => i.permutations).
+                Where(g => g.Count() == numberOfpermutations).
+                Select(g => g.Select(n => n.value).Min()).
+                Min();
+        }
+
+
+        private string ConvertToSmallestPermutationString(long number)
+        {
+            return  new String(number.ToString().ToArray().OrderBy(c => c).ToArray());
+        }
+
+
+    }
+}
---
+++ jae.euler.test/level03/E062CubicPermutationsUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace jae.euler.test.level03
+{
+    public class E062CubicPermutationsUnitTest
+    {
+
+
+
+     
+
+        [Fact]
+        public void Test_1()
+        {
+            /*
+                The cube, 41063625 (345^3), can be permuted to produce two other cubes: 56623104 (384^3) and 66430125 (405^3).
+                 In fact, 41063625 is the smallest cube which has exactly three permutations of its digits which are also cube.             
+             * 
+               */
+            var sut = new E062CubicPermutations();
+            Assert.Equal(41063625, sut.GetSmallestCube(numberOfpermutations: 3));
+
+
+        }
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+            Find the smallest cube for which exactly five permutations of its digits are cube.
+            */
+
+            var sut = new E062CubicPermutations();
+            Assert.Equal(127035954683, sut.GetSmallestCube(numberOfpermutations: 5));
+
+
+
+            /*
+              Congratulations, the answer you gave to problem 62 is correct.
+
+             You are the 27159th person to have solved this problem.
+            */
+        }
+    }
+
+
+}
