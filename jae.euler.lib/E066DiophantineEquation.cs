a93171d7d67507a07e5894ab7c708c45cccf1e9d
janarild.eileng <janarild.eileng@fhi.no>
2019-04-30 14:47:47 +0200

    E066DiophantineEquation pre

---
+++ jae.euler.lib/E066DiophantineEquation.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E066DiophantineEquation
+    {
+        /*
+         x^2 – Dy^2 = 1
+         */
+
+
+        public long GetDFromLargestXInMinimumSolution(int DMax)
+        {
+            long foundD = 0;
+            long largestX = 0;
+
+
+          for (int D=1;D<=DMax;D++)
+          {
+                if (IsSquare(D)) continue;
+                long x = GetXInMinimumSolution(D);
+                if (x > largestX)
+                {
+                    largestX = x;
+                    foundD = D;
+
+                }
+          }
+
+            return foundD;
+        }
+
+
+        public long GetXInMinimumSolution(int D)
+        {
+            const long MaxX = 10000000;
+            for (long y=2;y< MaxX;y++)
+            {
+
+                long r = D*y * y + 1;
+                if (IsSquare(r))
+                {
+                    return (long)Math.Sqrt(r);
+                }
+
+            }
+
+            return 0;
+        //  throw new Exception($"  x above {MaxX} D={D}");
+        }
+
+        public long GetXInMinimumSolution2(int D)
+        {
+            const long MaxX = 100000000;
+
+            long x = 2;
+            while (true)
+            {
+                long r = x * x - 1;
+
+                if (r % D == 0)
+                {
+                    long y2 = r / D;
+                    if (IsSquare(y2))
+                    {
+                        return x;
+
+                    }
+                }
+                x++;
+
+                if (x > MaxX) throw new Exception($"  x above {MaxX} D={D}");
+            }
+        }
+
+
+
+
+        private bool IsSquare(long n)
+        {
+            long d = (long)Math.Sqrt(n);
+            long d2 = (long)(d * d);
+            return d2 == n;
+        }
+
+     
+    }
+}
---
+++ jae.euler.test/level03/E066DiophantineEquationUnitTest.cs
@@ -0,0 +1,63 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace jae.euler.test.level03
+{
+    public class E066DiophantineEquationUnitTest
+    {
+        [Fact]
+        public void Test_MinimalSolution()
+        {
+            /*
+                For example, when D=13, the minimal solution in x is 649^2 – 13×180^2 = 1.           
+             * 
+               */
+            var sut = new E066DiophantineEquation();
+            Assert.Equal(649, sut.GetXInMinimumSolution(D:13));
+
+            Assert.Equal(31, sut.GetXInMinimumSolution(D: 60));
+            Assert.Equal(649, sut.GetXInMinimumSolution(D: 61));
+
+
+        }
+
+
+        [Fact]
+        public void Test_FindLargestXMinimalSolution()
+        {
+            /*
+                  minimal solutions in x for D ≤ 7, the largest x is obtained when D=5.         
+             * 
+               */
+            var sut = new E066DiophantineEquation();
+            Assert.Equal(5, sut.GetDFromLargestXInMinimumSolution(DMax:7));
+        }
+
+
+
+      
+
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+            Find the smallest cube for which exactly five permutations of its digits are cube.
+            */
+
+            var sut = new E066DiophantineEquation();
+            Assert.Equal(1, sut.GetDFromLargestXInMinimumSolution(DMax:1000));
+
+            /*
+              Congratulations, the answer you gave to problem 62 is correct.
+
+             You are the 27159th person to have solved this problem.
+            */
+        }
+    }
+}
