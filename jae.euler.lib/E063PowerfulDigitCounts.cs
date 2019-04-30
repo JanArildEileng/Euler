6f893d186a375e3db6ee576af9a82108c0c77567
janarild.eileng <janarild.eileng@fhi.no>
2019-04-30 08:53:38 +0200

    E063PowerfulDigitCounts

---
+++ jae.euler.lib/E063PowerfulDigitCounts.cs
@@ -0,0 +1,30 @@
+﻿using jae.euler.util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E063PowerfulDigitCounts
+    {
+     
+        public int GetNumberIntegerWhichAreNthPoweres()
+        {
+            int numberIntegerWhichAreNthPoweres = 0;
+            for(int x=1;x<10;x++)
+            {
+                List<int> powerNumber = new List<int> { x }; //larger than long
+
+                int nthPower = 1;
+                while (powerNumber.Count == nthPower)
+                {
+                    numberIntegerWhichAreNthPoweres++;
+                    powerNumber = DigitsList.Product(powerNumber, x);
+                    nthPower++;
+                }
+            }
+
+           return numberIntegerWhichAreNthPoweres;
+        }
+    }
+}
---
+++ jae.euler.test/level03/E063PowerfulDigitCountsUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace jae.euler.test.level03
+{
+    public class E063PowerfulDigitCountsUnitTest
+    {      
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+            How many n-digit positive integers exist which are also an nth power?
+            */
+
+            var sut = new E063PowerfulDigitCounts();
+            Assert.Equal(49, sut.GetNumberIntegerWhichAreNthPoweres());
+
+            /*
+              Congratulations, the answer you gave to problem 63 is correct.
+
+              You are the 37859th person to have solved this problem.
+            */
+        }
+    }
+}
