1fea9dcdc04213c1b02d0b89f6ed1bc2308c0633
janarild.eileng <janarild.eileng@fhi.no>
2019-05-08 13:56:43 +0200

    E069TotientMaximum

---
+++ jae.euler.lib/E069TotientMaximum.cs
@@ -0,0 +1,69 @@
+﻿using jae.euler.math;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E069TotientMaximum
+    {
+        List<long> primeFactors;
+        Dictionary<long, long> dict;
+
+        public E069TotientMaximum(int size)
+        {
+             primeFactors = Primes.GetPrimeFactorsBelowNumber((long)size);
+            dict = primeFactors.ToDictionary(p => p);
+
+        }
+
+
+        /*
+           Ser ut som  Euler's Totient function, φ(n)  gir max, 
+           når n er minste tall med maximalt antall prime-faktorer..
+
+            
+         */
+
+        public int GetNWithMaxTotient(int upperlimit)
+        {
+   
+            long product = 1;
+      
+            int i = 0;
+            while (true)
+            {
+                if (product * primeFactors[i] <= upperlimit)
+                {
+                    product *= primeFactors[i];
+                    i++;
+                }
+                else
+                    return (int)product;
+            }
+
+        }
+
+
+
+
+        /*
+         *  Euler's Totient function, φ(n) [sometimes called the phi function], is used to determine the number of numbers less than n which are relatively prime to n.
+         */
+
+
+        public int Totient(int n)
+        {
+            var primes = Primes.GetPrimeFactorsInNumber(n).Distinct();
+            return Enumerable.Range(1, n - 1).Where(e => !primes.Any(p => e % p == 0)).Count();
+
+        }
+
+
+
+
+
+        
+    }
+}
---
+++ jae.euler.test/level03/E069TotientMaximumUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+namespace jae.euler.test.level03
+{
+    public class E069TotientMaximumUnitTest
+    {
+        [Theory]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 2)]
+        [InlineData(5, 4)]
+        [InlineData(6, 2)]
+        [InlineData(7, 6)]
+        [InlineData(8, 4)]
+        [InlineData(9, 6)]
+        [InlineData(10,4)]
+
+        public void Test_Totient(int n,int phi)
+        {
+            var sut = new E069TotientMaximum(10);
+            Assert.Equal(phi, sut.Totient(n));
+        }
+
+
+        [Fact]
+        public void Test_1()
+        {
+            /*
+                 It can be seen that n=6 produces a maximum n/φ(n) for n ≤ 10.         
+            */
+            var sut = new E069TotientMaximum(10);
+            Assert.Equal(6, sut.GetNWithMaxTotient(upperlimit: 10));
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+            Find the value of n ≤ 1,000,000 for which n/φ(n) is a maximum.
+            */
+            int upperlimit = 1000000;
+
+
+            var sut = new E069TotientMaximum(upperlimit);
+            Assert.Equal(510510, sut.GetNWithMaxTotient(upperlimit: upperlimit));
+
+            /*
+            Congratulations, the answer you gave to problem 69 is correct.
+
+            You are the 30074th person to have solved this problem.
+            */
+        }
+    }
+}
