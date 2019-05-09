f2adc5d8223c53141352dfb88a2612d5061cd4f3
janarild.eileng <janarild.eileng@fhi.no>
2019-05-09 16:16:25 +0200

    E072CountingFractions

---
+++ jae.euler.lib/E072CountingFractions.cs
@@ -0,0 +1,14 @@
+﻿using jae.euler.math;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E072CountingFractions
+    {
+        public long GetNumberOfProperFractions(int dmax)
+        {
+            Totient totient = new Totient(dmax);
+            return  Enumerable.Range(2, dmax-1).Select(i => totient.Calc(i)).Sum();
+        }           
+    }
+}
---
+++ jae.euler.test/level03/E072CountingFractionsUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+namespace jae.euler.test.level03
+{
+    public class E072CountingFractionsUnitTest
+    {
+
+        [Fact]
+        public void Test_1()
+        {
+            var sut = new E072CountingFractions();
+            Assert.Equal(21, sut.GetNumberOfProperFractions(dmax: 8));
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+          How many elements would be contained in the set of reduced proper fractions for d ≤ 1,000,000?
+            */
+
+            var sut = new E072CountingFractions();
+            Assert.Equal(303963552391, sut.GetNumberOfProperFractions(dmax: 1000000));
+
+            /*
+              Congratulations, the answer you gave to problem 72 is correct.
+              You are the 19373rd person to have solved this problem.
+              This problem had a difficulty rating of 20%.
+            */
+        }
+    }
+}
