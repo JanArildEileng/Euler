838bc99d0a3c647c1677b89ca6028ab439f4ce4b
janarild.eileng <janarild.eileng@fhi.no>
2019-05-09 15:34:37 +0200

    E071OrderedFractions

---
+++ jae.euler.lib/E071OrderedFractions.cs
@@ -0,0 +1,42 @@
+﻿using jae.euler.math;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E071OrderedFractions
+    {
+        public long GetNumeratorLeftOf(int dmax, int rightNumerator, int rightDenominator)
+        {
+            Fraction rightFraction = new Fraction { Numerator = rightNumerator, Denominator = rightDenominator };
+            Fraction bestLeft = new Fraction { Numerator = 1, Denominator = dmax };
+
+
+            for (int i = 2; i <=dmax; i++){
+                //   bestleft <  a/i   < rightFraction
+                if (i == rightDenominator) continue;
+
+                long a_min = (bestLeft.Numerator * i / bestLeft.Denominator);
+                long a_max = (rightFraction.Numerator * i / rightFraction.Denominator);
+
+
+                if ( a_max > a_min)
+                {
+                    if (i % rightDenominator==0)
+                    {
+                        long div = i / rightDenominator;
+                        //ignore multiple of rightFraction;
+                        if (rightNumerator * div == a_max) continue;
+                    }
+
+                    bestLeft = new Fraction { Numerator = a_max, Denominator = i };
+                }
+            }
+
+            Fraction reduced = Fraction.ReduceFraction(bestLeft);
+            return reduced.Numerator;
+
+        }
+    }
+}
---
+++ jae.euler.test/level03/E071OrderedFractionsUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using jae.euler.lib;
+using Xunit;
+using jae.euler.math;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+namespace jae.euler.test.level03
+{
+    public class E071OrderedFractionsUnitTest
+    {
+
+        [Fact]
+        public void Test_1()
+        {
+            var sut = new E071OrderedFractions();
+            Assert.Equal(2, sut.GetNumeratorLeftOf(dmax: 8, rightNumerator: 3, rightDenominator: 7));
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+           By listing the set of reduced proper fractions for 
+           d ≤ 1,000,000 in ascending order of size, 
+           find the numerator of the fraction immediately to the left of 3/7.
+            */
+        
+
+
+            var sut = new E071OrderedFractions();
+            Assert.Equal(428570, sut.GetNumeratorLeftOf(dmax: 1000000, rightNumerator: 3, rightDenominator: 7));
+
+            /*
+              Congratulations, the answer you gave to problem 71 is correct.
+              You are the 25498th person to have solved this problem.
+             This problem had a difficulty rating of 10%. 
+            */
+        }
+    }
+}
