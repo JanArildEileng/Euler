5408452100ef6024040b22fddd1e27ec3ad8c418
janarild.eileng <janarild.eileng@fhi.no>
2019-05-15 12:47:06 +0200

    E067MaximumpathsumII

---
+++ jae.euler.test/level03/E067MaximumpathsumIIUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using jae.euler.lib;
+using System;
+using Xunit;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace jae.euler.test.level03
+{
+    public class E067MaximumpathsumIIUnitTest
+    {
+        private string[] GetTriangleFromFile()
+        {     
+           var triangleString = File.ReadAllText("DataFiles\\p067_triangle.txt");
+           return triangleString.Split(new char[] { '\n', '\r' }).Where(e => e.Length > 0).ToArray();
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+                Find the maximum total from top to bottom of the triangle below:
+            */
+    
+            var sut = new E018MaximumpathsumI();
+            Assert.Equal(7273, sut.GetMaximumpathsum(GetTriangleFromFile()));
+
+            /*
+               Congratulations, the answer you gave to problem 67 is correct.
+               You are the 86410th person to have solved this problem.
+               This problem had a difficulty rating of 5%. 
+            */
+        }
+    }
+}
