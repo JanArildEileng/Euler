ffa9c9a286ce3c1d3d1f24b3edc390e0eb21f5a8
janarild.eileng <janarild.eileng@fhi.no>
2019-05-16 16:49:05 +0200

    E076CountingSummations

---
+++ jae.euler.lib/E076CountingSummations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E076CountingSummations
+    {
+        int counter;
+
+        public int GetNumberOfDifferentWays(int sum)
+        {
+            counter = 0;
+            Recursive(rest: sum, level: 0);
+
+            return counter;
+        }
+
+
+
+        private void Recursive(int rest,int level)
+        {
+            if (rest==0)
+            {
+                counter += 1;
+                return;
+
+            } else
+            {
+                for(int i= rest; i>=0;i--)
+                {
+                    Recursive(rest - i, level + 1);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ jae.euler.test/level04/E076CountingSummationsUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using jae.euler.lib;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E076CountingSummationsUnitTest
+    {
+        [Fact]
+        public void Test_0()
+        {
+            /* It is possible to write five as a sum in exactly six different ways:
+
+            4 + 1
+            3 + 2
+            3 + 1 + 1
+            2 + 2 + 1
+            2 + 1 + 1 + 1
+            1 + 1 + 1 + 1 + 1 
+
+             */
+            var sut = new E076CountingSummations();
+            Assert.Equal(6, sut.GetNumberOfDifferentWays(sum: 5));
+
+
+        }
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+                 How many different ways can one hundred be written as a sum of at least two positive integers?     
+                 
+            */
+
+             var sut = new E076CountingSummations();
+             Assert.Equal(6, sut.GetNumberOfDifferentWays(sum: 100));
+
+            /*
+       
+            */
+        }
+    }
+}
