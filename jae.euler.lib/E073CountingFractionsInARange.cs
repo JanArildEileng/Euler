9c350f36388b28e404436f06f113a3470c86c639
janarild.eileng <janarild.eileng@fhi.no>
2019-05-16 07:48:27 +0200

    E073CountingFractionsInARange

--- jae.euler.lib/E073CountingFractionsInARange.cs
+++ jae.euler.lib/E073CountingFractionsInARange.cs
@@ -1,40 +1,64 @@
 ﻿using jae.euler.math;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace jae.euler.lib
 {
     public class E073CountingFractionsInARange
     {
-        public long GetNumberOfFractionsBetween(int dmax, int leftNumerator, int leftDenominator, int rightNumerator, int rightDenominator)
+        public long GetNumberOfFractionsBetween(int denominatorMax, int leftNumerator, int leftDenominator, int rightNumerator, int rightDenominator)
         {
             long numberOfFractionsBetween = 0;
             Fraction leftFraction = new Fraction { Numerator = leftNumerator, Denominator = leftDenominator };
             Fraction rightFraction = new Fraction { Numerator = rightNumerator, Denominator = rightDenominator };
 
 
-            for (int i = 2; i <= dmax; i++)
+            for (int denominator = 2; denominator <= denominatorMax; denominator++)
             {
-                //   bestleft <  a/i   < rightFraction
-                //    if (i == rightDenominator) continue;
-                if (i == leftFraction.Denominator || i == rightFraction.Denominator) continue;
+                // finn  interval av numerator slik at   leftFraction < numeratorStart/denominator  &&     numeratorEnd/denominator < rightFraction
 
+               long numeratorStart = (long)Math.Ceiling((double)(leftFraction.Numerator * 1.0* denominator / leftFraction.Denominator));
+               long numeratorEnd = (rightFraction.Numerator * denominator / rightFraction.Denominator);
 
-               long  a_min = (leftFraction.Numerator * i / leftFraction.Denominator);
-               long  a_max = (rightFraction.Numerator * i / rightFraction.Denominator);
+                if (numeratorEnd >= numeratorStart) { 
 
-               for (long l= a_min+1;l<=a_max;l++ )
-                {
-                    numberOfFractionsBetween += 1;
+                    //border cases
+                    if (denominator % rightDenominator == 0)
+                    {
+                        long div = denominator / rightDenominator;
+                        if (rightNumerator * div == numeratorEnd) numeratorEnd--;
+                    }
 
+                    if (denominator % leftDenominator == 0)
+                    {
+                        long div = denominator / leftDenominator;
+                        if (leftNumerator * div == numeratorStart) numeratorStart++;
+                    }
+
+                     numberOfFractionsBetween += GetNoneReducableInInterval(numeratorStart, numeratorEnd, denominator);
                 }
-
             }
-
-         //   Fraction reduced = Fraction.ReduceFraction(bestLeft);
             return numberOfFractionsBetween;
-
         }
+
+
+        //hvis Numerator har felles primetall med  denominator, kan brøken reduseres..
+        private int GetNoneReducableInInterval(long numeratorStart, long numeratorEnd, int denominator)
+        {
+            int counter = 0;
+
+            List<long> PrimeFactorsInDenominator = Primes.GetPrimeFactorsInNumber(denominator).ToList();
+
+            for (long numerator = numeratorStart; numerator <= numeratorEnd; numerator++)
+            {
+               if ( !PrimeFactorsInDenominator.Any(p=> numerator % p==0))
+                    counter++;
+            }
+            return counter;
+        }
+
+
     }
 }
--- jae.euler.test/level03/E073CountingFractionsInARangeUnitTest.cs
+++ jae.euler.test/level03/E073CountingFractionsInARangeUnitTest.cs
@@ -1,10 +1,5 @@
 ﻿using jae.euler.lib;
 using Xunit;
-using jae.euler.math;
-using System.Linq;
-using System.Collections.Generic;
-using System.IO;
-using System.Numerics;
 
 namespace jae.euler.test.level03
 {
@@ -15,17 +10,13 @@ namespace jae.euler.test.level03
         public void Test_1()
         {
             /*
-             If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size, we get:
-
-             1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
-
-             It can be seen that there are 3 fractions between 1/3 and 1/2. 
-             
+               If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size, we get:
+               1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+               It can be seen that there are 3 fractions between 1/3 and 1/2.             
              */
 
-
             var sut = new E073CountingFractionsInARange();
-            Assert.Equal(3, sut.GetNumberOfFractionsBetween(dmax: 8, leftNumerator: 1, leftDenominator:3,rightNumerator: 1, rightDenominator: 2));
+            Assert.Equal(3, sut.GetNumberOfFractionsBetween(denominatorMax: 8, leftNumerator: 1, leftDenominator: 3, rightNumerator: 1, rightDenominator: 2));
         }
 
         [Fact]
@@ -36,15 +27,15 @@ namespace jae.euler.test.level03
                in the sorted set of reduced proper fractions for d ≤ 12,000?
             */
 
-
-
             var sut = new E073CountingFractionsInARange();
-            Assert.Equal(3, sut.GetNumberOfFractionsBetween(dmax: 12000, leftNumerator: 1, leftDenominator: 3, rightNumerator: 1, rightDenominator: 2));
-        
+            Assert.Equal(7295372, sut.GetNumberOfFractionsBetween(denominatorMax: 12000, leftNumerator: 1, leftDenominator: 3, rightNumerator: 1, rightDenominator: 2));
 
-        /*
-          
-        */
+            /*
+              Congratulations, the answer you gave to problem 73 is correct.
+              You are the 21983rd person to have solved this problem.
+              Nice work, janei, you've just advanced to Level 3 . 
+              21724 members (2.39%) have made it this far.
+            */
+        }
     }
-}
 }
