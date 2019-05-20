417ff2d5b9f6ddd60d27b16bea82e73ff740582d
janarild.eileng <janarild.eileng@fhi.no>
2019-05-20 08:42:11 +0200

    E076CountingSummations

--- jae.euler.lib/E076CountingSummations.cs
+++ jae.euler.lib/E076CountingSummations.cs
@@ -1,39 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace jae.euler.lib
 {
     public class E076CountingSummations
-    {
-        int counter;
+    {     
+        /*
+         Hvor mange addisjoner inneholder minst like mange '1' som neste tall som skal brukes til å lage flere addisjoner.. 
+          
+         */ 
 
         public int GetNumberOfDifferentWays(int sum)
         {
-            counter = 0;
-            Recursive(rest: sum, level: 0);
+            var tails = new List<int> { sum };
+            int counter = 1;
 
+            //loop gjennom alle tall opptil n
+            for (int i=2;i<sum;i++)
+            {
+                tails = tails.Where(t => t >= i).ToList(); 
+
+                int index = 0;
+                while ( index < tails.Count)
+                {
+                    var restAntall = tails[index]-i;
+
+                    if (restAntall >= 0  )
+                    {
+                        counter++;
+                        if (restAntall >= i)
+                        tails.Add(restAntall);
+                    }
+                    index++;
+                }
+            }
+       
             return counter;
         }
 
 
+        //public int GetNumberOfDifferentWays2(int sum)
+        //{
 
-        private void Recursive(int rest,int level)
-        {
-            if (rest==0)
-            {
-                counter += 1;
-                return;
+        //    List<List<int>> tails = new List<List<int>>();
+        //    tails.Add(Enumerable.Repeat(1, sum).ToList());
 
-            } else
-            {
-                for(int i= rest; i>=0;i--)
-                {
-                    Recursive(rest - i, level + 1);
-                }
 
-            }
+        //    int counter = 1;
+        //    //loop gjennom alle tall opptil n
+        //    for (int i = 2; i < sum; i++)
+        //    {
 
-        }
+        //        tails = tails.Where(t => t.Count() >= i).ToList();
+
+
+        //        int index = 0;
+        //        while (index < tails.Count)
+        //        {
+        //            var list = tails[index];
+
+        //            if (list.Count >= i)
+        //            {
+        //                counter++;
+        //                if ((list.Count - i) >= i)
+        //                    tails.Add(Enumerable.Repeat(1, list.Count - i).ToList());
+        //            }
+        //            index++;
+        //        }
+        //    }
+
+        //    return counter;
+        //}
+
+
     }
 }
--- jae.euler.test/level04/E076CountingSummationsUnitTest.cs
+++ jae.euler.test/level04/E076CountingSummationsUnitTest.cs
@@ -20,7 +20,9 @@ namespace jae.euler.test.level04
              */
             var sut = new E076CountingSummations();
             Assert.Equal(6, sut.GetNumberOfDifferentWays(sum: 5));
-
+            Assert.Equal(10, sut.GetNumberOfDifferentWays(sum: 6));
+            Assert.Equal(14, sut.GetNumberOfDifferentWays(sum: 7));
+            Assert.Equal(21, sut.GetNumberOfDifferentWays(sum: 8));
 
         }
 
@@ -34,10 +36,12 @@ namespace jae.euler.test.level04
             */
 
              var sut = new E076CountingSummations();
-             Assert.Equal(6, sut.GetNumberOfDifferentWays(sum: 100));
+             Assert.Equal(190569291, sut.GetNumberOfDifferentWays(sum: 100));
 
             /*
-       
+            Congratulations, the answer you gave to problem 76 is correct.
+            You are the 25052nd person to have solved this problem.
+            This problem had a difficulty rating of 10%
             */
         }
     }
