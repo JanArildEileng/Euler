18d6eb0973102acec076447c5e91e45183296d21
janarild.eileng <janarild.eileng@fhi.no>
2019-05-21 13:46:48 +0200

    E077PrimeSummations

---
+++ jae.euler.lib/E077PrimeSummations.cs
@@ -0,0 +1,67 @@
+﻿using jae.euler.math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E077PrimeSummations
+    {
+        int Size;
+        List<long> primliste;
+        Dictionary<long, long> dictionary;
+
+        public E077PrimeSummations(int size)
+        {
+            this.Size = size;
+            primliste = Primes.GetPrimeFactorsBelowNumber(size);
+            dictionary = primliste.ToDictionary(e=>e);
+            primliste.Reverse(); 
+        }
+
+        //alle tall ( >1)  kan skrives som sum av 2 og 3
+
+        public int GetNumberOfDifferentWays(long number,long maxPrime)
+        {
+            int counter = 0;
+
+            var pliste = primliste.Where(p => p <= maxPrime);      
+
+            foreach(var p in pliste)
+            {
+                var rest = number - p;
+                if (rest < 2) continue;
+                if (p == 2 && number % 2 != 0) continue;
+                if (p == 2 && number % 2 == 0)
+                {
+                    counter++;
+                    continue;
+                }
+
+                if (dictionary.ContainsKey(rest) && rest <= p)
+                    counter++;                    
+    
+                if (rest > 3)
+                    counter += GetNumberOfDifferentWays(rest,rest<p?rest:p );            
+            }
+
+            return counter;
+        }
+
+        public int GetFirstNumberWithMoreThanPrimeSummations(int moreThanWays)
+        {
+
+            for(int i=2;i<=Size;i++)
+            {
+                var numberOfDifferentWays = GetNumberOfDifferentWays(i,Size);
+                if (numberOfDifferentWays > moreThanWays)
+                {
+                    return i;
+                }
+            }
+
+            throw new Exception($"Didnt dont find {moreThanWays} in {Size }");
+        }
+
+    }
+}
---
+++ jae.euler.test/level04/E077PrimeSummationsUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using jae.euler.lib;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E077PrimeSummationsUnitTest
+    {
+        /* 
+                 It is possible to write ten as the sum of primes in exactly five different ways:
+                   7 + 3
+                   5 + 5
+                   5 + 3 + 2
+                   3 + 3 + 2 + 2
+                   2 + 2 + 2 + 2 + 2
+        */
+
+        [Theory]
+        [InlineData(5,1)]
+        [InlineData(6, 2)]
+        [InlineData(7, 2)]
+        [InlineData(8, 3)]
+        [InlineData(9, 4)]
+        [InlineData(10, 5)]
+        [InlineData(13, 7)]
+        public void Test_0(int number, int expectedResult)
+        {
+            
+            var sut = new E077PrimeSummations(number);
+            Assert.Equal(expectedResult, sut.GetNumberOfDifferentWays(number, maxPrime: 10));
+         }   
+
+        [Fact]
+        public void Test_1()
+        {          
+            var sut = new E077PrimeSummations(100);
+            Assert.Equal(5006, sut.GetNumberOfDifferentWays(71,maxPrime:100));
+        }
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+                What is the first value which can be written as the sum of primes in over five thousand different ways?     
+            */
+
+             var sut = new E077PrimeSummations(200);
+             Assert.Equal(71, sut.GetFirstNumberWithMoreThanPrimeSummations(5000));
+
+            /*
+                Congratulations, the answer you gave to problem 77 is correct.
+                You are the 16527th person to have solved this problem.
+                This problem had a difficulty rating of 25%.
+            */
+        }
+    }
+}
