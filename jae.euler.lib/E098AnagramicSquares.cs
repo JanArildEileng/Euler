7083cf13e712215510cfb7cd0ed1427fc32fc930
janarild.eileng <janarild.eileng@fhi.no>
2019-05-22 21:16:14 +0200

    E098AnagramicSquares pre

---
+++ jae.euler.lib/E098AnagramicSquares.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E098AnagramicSquares
+    {
+
+        class subStruct
+        {
+            public long value { get; set; }
+            public List<int> selected { get; set; } 
+
+
+           public Dictionary<char,Mapping> mapping { get; set; }
+
+        }
+
+        class Mapping
+        {
+            public char c  { get; set; }
+            public int value  { get; set; }
+        }
+
+
+        public long GetLargestSqureNumber(List<string> words)
+        {
+            long maxSquareNumber = 0;
+
+            foreach(var word in words)
+            {
+                long max = GetSquareNumber(word, words);
+
+                if (max > maxSquareNumber)
+                    maxSquareNumber = max;
+            }
+            return maxSquareNumber;
+
+        }
+
+        public long GetSquareNumber(string word, List<string> words)
+        {
+
+
+            long maxSquareNumber = 0;
+            List<subStruct> SquareNumbers=new List<subStruct>();
+            var warray = word.ToCharArray();
+
+            Action<subStruct> action = (a) =>
+            {
+                SquareNumbers.Add(a);
+            };
+
+
+
+            int[] intvalues = Enumerable.Range(1,11).ToArray();
+
+            Recursive(intvalues.ToList(), new List<int>(), warray, action);
+
+
+
+            //int i = 0;
+            //var darray = warray.Distinct().Select(c => new { a = c, b = intvalues[i++] }).ToDictionary(e => e.a);
+
+
+            //long value = 0;
+
+            //int[] s =warray.Select(c => darray[c].b).ToArray();
+
+            //StringBuilder builder = new StringBuilder();
+            //warray.Select(c => darray[c].b).ToList().ForEach(a => builder.Append(a));
+
+
+            //value= long.Parse(builder.ToString());
+
+            //if (IsSquare(value))
+            //    return value;
+
+            foreach (var testword in words)
+            {
+
+                if (testword.Equals(word)) continue;
+
+                var w2 = testword.ToCharArray();
+                var w2d = w2.Distinct().Count();
+
+                foreach (var sub in SquareNumbers)
+                {
+                    bool isMatch = true;
+                    if (sub.selected.Count != w2d) break;
+                        //Action<subStruct> action2 = (a) =>
+                        //{
+                        //    isMatch = true; ;
+                        //};
+
+
+                        //if (sub.selected.Count == w2d)
+                        //    Recursive(null, sub.selected, w2, action2);
+
+
+
+                        if (isMatch)
+                    {
+
+                        try
+                        {
+                            StringBuilder builder = new StringBuilder();
+                            w2.Select(c => sub.mapping[c].value).ToList().ForEach(a => builder.Append(a));
+
+                            long value = long.Parse(builder.ToString());
+                            if (IsSquare(value))
+                            {
+                                maxSquareNumber = Math.Max(value, sub.value);
+
+                            }
+
+
+
+                        }
+                        catch (Exception)
+                        {
+
+                            break;
+                        }
+                     
+                    }
+
+                }
+
+
+            }
+
+
+        
+
+
+
+         
+            return maxSquareNumber;
+          
+        }
+
+
+        private  void Recursive(List<int> canddates, List<int> selected,char[] word, Action<subStruct> action)
+        {
+            if (selected.Count == word.Distinct().Count())
+            {
+
+              
+
+
+
+                int i =0;
+                var mapping = word.Distinct().Select(c => new Mapping { c = c, value = selected[i++] }).ToDictionary(e => e.c);
+
+                long value = 0;
+
+          
+
+                StringBuilder builder = new StringBuilder();
+                word.Select(c => mapping[c].value).ToList().ForEach(a => builder.Append(a));
+
+                try
+                {
+                    value = long.Parse(builder.ToString());
+                }
+                catch (Exception)
+                {
+
+            //        throw;
+                }
+             
+
+
+
+                if (IsSquare(value))
+                {
+                    action(new subStruct {value=value,selected= selected ,mapping= mapping });
+
+                }
+
+                    return;
+            }
+                
+
+
+            foreach(var candiate in canddates)
+            {
+                var a = selected.ToList();
+                a.Add(candiate);
+                var subcanddates = canddates.Except(new List<int> { candiate }).ToList();
+                Recursive(subcanddates, a, word,action);
+            }
+
+
+        }
+
+
+
+
+
+
+        private bool IsSquare(long number)
+        {
+            long a = (long)Math.Sqrt(number);
+            return (a * a == number);
+
+        }
+    }
+}
---
+++ jae.euler.test/level04/E098AnagramicSquaresUnitTest.cs
@@ -0,0 +1,97 @@
+﻿using jae.euler.lib;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E098AnagramicSquaresUnitTest
+    {
+
+
+        private List<string> GetWordsFromFile()
+        {
+            /*
+                Using names.txt (right click and 'Save Link/Target As...'),
+                a 46K text file containing over five-thousand first names, 
+            */
+
+            var filecontents = File.ReadAllText("DataFiles\\p098_words.txt");
+            var nameListe = filecontents.Split(new char[] { ',' });
+            var names = nameListe.ToList().Select(e => e.Replace('\"', ' ').Replace('"', ' ').Trim()).ToList();
+            return names;
+        }
+
+
+        [Fact]
+        public void Test_FileRead()
+        {
+
+            var words = GetWordsFromFile();
+            Assert.Equal(1786, words.Count());
+            Assert.Equal(11, words.Select(w=>w.ToCharArray().Distinct().Count() ).Max());
+
+            Assert.Equal("RESPONSIBILITY", words.OrderBy(w => w.ToCharArray().Distinct().Count()).Last());
+
+          
+
+            Assert.Equal("RESPONSIBILITY", "RESPONSIBILITY".ToCharArray().Distinct().ToString());
+        }
+
+
+
+        [Fact]
+        public void Test_CARE()
+        {
+            /*
+             *     CARE with 1, 2, 9, and 6 respectively, we form a square number: 1296 = 362.
+             */
+            var words = GetWordsFromFile();
+            var sut = new E098AnagramicSquares();
+            Assert.Equal(9216, sut.GetSquareNumber("CARE", words));
+        }
+
+
+
+        [Fact]
+        public void Test_ADMINISTRATION()
+        {
+           
+            var words = GetWordsFromFile();
+            var sut = new E098AnagramicSquares();
+            Assert.Equal(9216, sut.GetSquareNumber("ADMINISTRATION", words));
+        }
+        
+
+
+
+        [Fact]
+        public void Test_RESPONSIBILITY()
+        {
+            /*
+             *     CARE with 1, 2, 9, and 6 respectively, we form a square number: 1296 = 362.
+             */
+
+            var sut = new E098AnagramicSquares();
+        
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+               What is the largest square number formed by any member of such a pair?
+            */
+            var words = GetWordsFromFile();
+            var sut = new E098AnagramicSquares();
+            Assert.Equal(-1, sut.GetLargestSqureNumber(words));
+
+            /*
+                Congratulations, the answer you gave to problem 77 is correct.
+                You are the 16527th person to have solved this problem.
+                This problem had a difficulty rating of 25%.
+            */
+        }
+    }
+}
