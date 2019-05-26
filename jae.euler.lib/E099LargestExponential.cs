ad494fd918bb54bf23cf64b3f82750dcef8d86f8
janarild.eileng <janarild.eileng@fhi.no>
2019-05-26 11:02:31 +0200

    E099LargestExponential

---
+++ jae.euler.lib/E099LargestExponential.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace jae.euler.lib
+{
+    public class E099LargestExponential
+    {
+        public int GetLineWithLargestNumber(List<string> expNumbers)
+        {
+            int indexLargestNumber = -1;
+            double largestNumber = 0.0; ;
+
+            for(int i=0;i< expNumbers.Count;i++)
+            {
+                var currentNumber = ToLogValue(expNumbers[i].Split(new char[] { ',' }));
+                if (currentNumber> largestNumber)
+                {
+                    indexLargestNumber = i;
+                    largestNumber = currentNumber;
+                }
+            }
+
+            return indexLargestNumber+1; //linenr =indx+1
+        }
+
+
+        private double  ToLogValue(string[] expNumber)
+        {
+            double a = double.Parse(expNumber[0]);
+            double b = double.Parse(expNumber[1]);
+            double c = b*Math.Log10(a);
+            return c;
+        }
+    }
+}
---
+++ jae.euler.test/level04/E099LargestExponentialUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using jae.euler.lib;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E099LargestExponentialUnitTest
+    {
+
+        private List<string> GetExpNumbersFromFile()
+        {
+            var filecontents = File.ReadAllLines("DataFiles\\p099_base_exp.txt");         
+            return filecontents.ToList();
+        }
+
+
+        [Fact]
+        public void Test_GetExpNumbersFromFile()
+        {
+            var lines = GetExpNumbersFromFile();
+            Assert.Equal(1000, lines.Count());    
+        }
+
+
+        [Fact]
+        public void Test_01()
+        {
+            /*
+             *   
+                632382,518061 > 519432,525806
+             */
+            var sut = new E099LargestExponential();
+            var expNumbers = new List<string> { "519432,525806", "632382,518061" };
+            Assert.Equal(2, sut.GetLineWithLargestNumber(expNumbers));
+        }
+      
+        [Fact]
+        public void Solution()
+        {
+            /*
+               determine which line number has the greatest numerical value.
+            */
+            var expNumbers = GetExpNumbersFromFile();
+            var sut = new E099LargestExponential();
+            Assert.Equal(709, sut.GetLineWithLargestNumber(expNumbers));
+
+
+            /*
+                Congratulations, the answer you gave to problem 99 is correct.
+                You are the 26931st person to have solved this problem.
+                This problem had a difficulty rating of 10%. 
+            */
+        }
+    }
+}
