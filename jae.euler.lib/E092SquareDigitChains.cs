366d8d6e8abd5e7ee00b74a42997813fd417848d
jan eileng <jaeileng@gmail.com>
2019-06-01 12:24:08 +0200

    E092SquareDigitChains

---
+++ jae.euler.lib/E092SquareDigitChains.cs
@@ -0,0 +1,71 @@
+﻿using jae.euler.util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E092SquareDigitChains
+    {
+        
+        Dictionary<int, bool> dict = new Dictionary<int, bool>(1000000);
+
+        public int GetCount(int below, int arrivat)
+        {
+            int count = 0;
+            for(int i=2;i<below;i++)
+            {
+                if (IsArrivingAt(i, arrivat))
+                    count++;
+            }
+            return count;
+        }
+
+        public  bool IsArrivingAt(int number, int arrivat)
+        {
+            List<int> list = new List<int>(10);
+            if (number <1000)
+            list.Add(number);
+
+         
+            while (true)
+            {
+                if (dict.TryGetValue(number, out bool t))
+                {
+                    return t;
+                }
+
+                number = SumSquareDigits(number);
+              //  if (number > 1000) throw new Exception($"number to big {number}");           
+
+                if (number == arrivat)
+                {
+                    foreach (var l in list)
+                        dict.Add(l, true);
+                    return true;
+                }
+
+                if (number == 1)
+                {
+                    foreach (var l in list)
+                        dict.Add(l, false);
+                    return false;
+                }
+         
+                list.Add(number);
+            }
+
+    
+        }
+
+
+        private int SumSquareDigits(int number)
+        {
+            List<int> list = DigitsList.ConvertToDigitListe(number);
+            return list.Select(e => e * e).Sum();
+        }
+
+
+    }
+}
---
+++ jae.euler.test/level04/E092SquareDigitChainsUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using jae.euler.lib;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E092SquareDigitChainsUnitTest
+    {
+        [Fact]
+        public void Test_IsArrivaingAt()
+        {
+         
+            var sut = new E092SquareDigitChains();
+
+            Assert.False(sut.IsArrivingAt(44, 89));
+            Assert.True(sut.IsArrivingAt(85, 89));
+
+        }
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+                    How many starting numbers below ten million will arrive at 89?                 
+            */
+
+            var sut = new E092SquareDigitChains();
+             Assert.Equal(8581146, sut.GetCount(below: 10000000, arrivat:89));
+
+            /*
+               Congratulations, the answer you gave to problem 92 is correct.
+                You are the 37168th person to have solved this problem.
+                This problem had a difficulty rating of 5%.
+            */
+        }
+    }
+}
