4324eb7f992be4dc02adcb4ea1771621b76e0429
janarild.eileng <janarild.eileng@fhi.no>
2019-06-03 08:27:49 +0200

    E081PathSumTwoWays solved

---
+++ jae.euler.lib/E081PathSumTwoWays.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E081PathSumTwoWays
+    {
+
+        int[,] minMatrix;
+
+        public int GetMinimalPathSum(int[][] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix[0].GetLength(0);
+
+            FillMinMatrix(matrix);
+
+
+            int x = rows-1;
+            int y = cols-1;
+            int counter = minMatrix[y,x];
+
+
+         
+           
+
+
+            return counter;
+        }
+
+
+        private void FillMinMatrix(int[][] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix[0].GetLength(0);
+
+            minMatrix = new int[rows, cols];
+
+            int y = 0;
+            int x = 0;
+            minMatrix[0, 0] = matrix[0][0];
+
+            for (x = 1; x < cols; x++)
+                minMatrix[y, x] = matrix[y][x] + minMatrix[y,x-1];
+            x = 0;
+            for (y = 1; y < rows; y++)
+                minMatrix[y, x] = matrix[y][x]+ minMatrix[y-1,x];
+
+            for(y=1;y<rows;y++)
+            {
+                for(x=1;x<cols;x++)
+                {
+                    minMatrix[y, x] = minMatrix[y - 1,x] < minMatrix[y,x - 1] ? minMatrix[y - 1,x] : minMatrix[y,x - 1];
+                    minMatrix[y, x] += matrix[y][x];
+                }
+            }
+
+
+
+        }
+
+    }
+}
---
+++ jae.euler.test/level04/E081PathSumTwoWaysUnitTest.cs
@@ -0,0 +1,64 @@
+﻿using jae.euler.lib;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E081PathSumTwoWaysUnitTest
+    {
+
+        private int[][] ReadMatrix()
+        {
+            int[][] matrix = new int[80][];
+
+            int i = 0;
+            foreach (var line in File.ReadAllLines(@"DataFiles\p081_matrix.txt"))
+            {
+                matrix[i++] = line.Split(',').Select(e => int.Parse(e)).ToArray();
+            }
+
+            return matrix;
+        }
+
+
+
+        [Fact]
+        public void Test_0()
+        {
+            int[][] matrix =
+            {
+                new int[] {131,673,234,103,18 },
+                new int[] {201,96,342,965,150 },
+                new int[] {630,803,746,422,111},
+                new int[] {537,699,497,121,956},
+                new int[] {805,732,524,37,331}
+            };
+
+            var sut = new E081PathSumTwoWays();
+            Assert.Equal(2427, sut.GetMinimalPathSum(matrix));
+         }
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+              Find the minimal path sum,
+              from the top left to the bottom right by only moving right and down.
+            */
+
+            var matrix = ReadMatrix();
+
+            var sut = new E081PathSumTwoWays();
+            Assert.Equal(427337, sut.GetMinimalPathSum(matrix));
+
+            /*
+                Congratulations, the answer you gave to problem 81 is correct.
+                You are the 30534th person to have solved this problem.
+                This problem had a difficulty rating of 10%.
+            */
+        }
+    }
+}
