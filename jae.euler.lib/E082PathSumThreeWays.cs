9a8c0f3586b41f363f761370e816e0d6bbe5faed
janarild.eileng <janarild.eileng@fhi.no>
2019-06-04 08:29:08 +0200

    E082PathSumThreeWays

---
+++ jae.euler.lib/E082PathSumThreeWays.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{  
+    public class E082PathSumThreeWays
+    {
+        enum Direction { up = -1, down = 1 };
+        int[,] matrix;
+        int[,] minMatrix;
+        int cols,rows;    
+
+        public int GetMinimalPathSum(int[,] matrix)
+        {
+            this.matrix = matrix;
+            rows = matrix.GetLength(0);
+            cols = matrix.GetLength(1);
+            minMatrix = new int[rows, cols];
+
+            FillMinMatrix();
+
+            //finn minste verdi i høyre kolonne
+            int minimalPathSum = minMatrix[0, cols - 1];
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (minMatrix[i, cols - 1] < minimalPathSum)
+                    minimalPathSum = minMatrix[i, cols - 1];
+            }
+
+            return minimalPathSum;
+        }
+
+        private void  FillMinMatrix()
+        {
+            for (int j = 0; j < rows; j++)
+                minMatrix[j, 0] = matrix[j, 0];
+            //for all columns (>0) :
+
+            for(int j=1;j<cols;j++)
+            {
+                //for all row in column:
+                for(int i=0;i<rows;i++)
+                {
+                    minMatrix[i, j] = (new List<int> {
+                         minMatrix[i, j - 1],
+                         PathValue(i, j,Direction.up),
+                         PathValue(i, j,Direction.down) }
+                    ).Where(e => e > 0).Min() 
+                    + matrix[i, j];
+                }
+            }
+        }
+
+
+        private int PathValue(int rad, int col, Direction direction)
+        {
+            int y = rad + (int)direction;
+            if (y == rows || y<0) return -1;
+
+            int path1 = minMatrix[y, col - 1] + matrix[y, col];
+            int pathValue = PathValue(y, col, direction);
+            if (pathValue < 0) return path1;
+            else
+            {
+                int path2 = pathValue + +matrix[y, col];
+                return path1 < path2 ? path1 : path2;
+            }
+        }       
+    }
+}
---
+++ jae.euler.test/level04/E082PathSumThreeWaysUnitTest.cs
@@ -0,0 +1,69 @@
+﻿using jae.euler.lib;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E082PathSumThreeWaysUnitTest
+    {
+     
+        private int[,] ReadMatrix()
+        {
+            int[,] matrix = new int[80,80];
+
+            int y = 0;
+            foreach (var line in File.ReadAllLines(@"DataFiles\p082_matrix.txt"))
+            {
+                int x = 0;
+                foreach(int number  in line.Split(',').Select(e => int.Parse(e)))
+                {  
+                    matrix[y, x++] = number;
+                }
+                y++;
+            }
+
+            return matrix;
+        }
+  
+
+        [Fact]
+        public void Test_1()
+        {
+            int[,] matrix =
+            {
+                {131,673,234,103,18 },
+                {201,96,342,965,150 },
+                {630,803,746,422,111},
+                {537,699,497,121,956},
+                {805,732,524,37,331}
+            };
+
+            var sut = new E082PathSumThreeWays();
+            Assert.Equal(994, sut.GetMinimalPathSum(matrix));
+        }
+
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+              Find the minimal path sum,
+              from the top left to the bottom right by only moving up, down, and right,.
+            */
+
+            var matrix = ReadMatrix();
+
+            var sut = new E082PathSumThreeWays();
+            Assert.Equal(260324, sut.GetMinimalPathSum(matrix));
+
+            /*
+               Congratulations, the answer you gave to problem 82 is correct.
+               You are the 18541st person to have solved this problem.
+               This problem had a difficulty rating of 20%. 
+            */
+        }
+    }
+}
