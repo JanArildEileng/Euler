2ed1345547e5b3b7f31760799e76e540cea3b5da
janarild.eileng <janarild.eileng@fhi.no>
2019-06-04 14:53:11 +0200

    E083PathSumFourWays

---
+++ jae.euler.lib/E083PathSumFourWays.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{  
+    public class E083PathSumFourWays
+    {
+        enum Direction { up = -1, down = 1 };
+        int[,] matrix;
+        int[,] minMatrix;
+        int cols,rows;
+
+
+    //    top left to the bottom right,
+
+        public int GetMinimalPathSum(int[,] matrix)
+        {
+            this.matrix = matrix;
+            rows = matrix.GetLength(0);
+            cols = matrix.GetLength(1);
+            minMatrix = new int[rows, cols];
+
+            FillMinMatrix();
+
+            //finn minste verdi i høyre kolonne
+            int minimalPathSum = minMatrix[rows-1, cols - 1];
+
+            return minimalPathSum;
+        }
+
+
+
+        private void FillMinMatrix2()
+        {
+            minMatrix[0, 0] = matrix[0, 0];
+
+            for (int j = 0; j < cols; j++)
+            {
+                //for all column
+                for (int i = 0; i < rows; i++)
+                {
+                    if ( i==0 && j>0)
+                        minMatrix[j, i] = minMatrix[j-1, i]+ matrix[j, i]+1;
+                    if (i>  0)
+                        minMatrix[j, i] = minMatrix[j, i-1] + matrix[j, i]+1;
+
+                }
+            }
+          
+            Recalc(0, 0);
+
+        }
+
+            private void  FillMinMatrix()
+        {
+             minMatrix[0, 0] = matrix[0, 0];
+             minMatrix[1, 0] = matrix[1, 0]+ matrix[0, 0];
+             minMatrix[0, 1] = matrix[0, 1]+ matrix[0, 0];
+
+            //for all row :
+            for (int j = 0; j < cols; j++)
+            {
+                //for all column
+                for (int i = 0; i < rows; i++)
+                {
+                    if ((i == 0 && j == 0) || (i == 1 && j == 0) || (i == 0 && j == 1)) continue;
+
+                    if ( i==0)
+                    {
+                        minMatrix[i, j] = minMatrix[i, j - 1]+ matrix[i, j];
+                    }
+                    if (j == 0)
+                    {
+                        minMatrix[i, j] = minMatrix[i-1, j] + matrix[i, j];
+                    }
+
+                    if ( j >0 && i >0 )
+                    {
+
+                        int left = minMatrix[i, j - 1];
+                        int up = minMatrix[i - 1, j];
+                        minMatrix[i, j] = matrix[i, j] + (left < up ? left : up);
+                    }
+
+                    Recalc(i, j);
+                }
+            }
+
+
+
+        }
+
+        private void Recalc(int i, int j)
+        {
+
+
+            if ( j >0 &&   minMatrix[i, j - 1] > (minMatrix[i, j] + matrix[i, j - 1]))
+            {
+                minMatrix[i, j - 1] = minMatrix[i, j] + matrix[i, j - 1];
+                Recalc(i, j - 1);
+            }
+            if (i >0 && minMatrix[i - 1, j] > (minMatrix[i, j] + matrix[i - 1, j]))
+            {
+                minMatrix[i - 1, j] = minMatrix[i, j] + matrix[i - 1, j];
+                Recalc(i-1, j );
+            }
+
+
+            if ( (j < cols-1)  && minMatrix[i, j + 1] > (minMatrix[i, j] + matrix[i, j + 1]))
+            {
+                minMatrix[i, j + 1] = minMatrix[i, j] + matrix[i, j + 1];
+                Recalc(i, j + 1);
+            }
+
+
+            if ((i < rows - 1) && minMatrix[i + 1, j] > (minMatrix[i, j] + matrix[i + 1, j]))
+            {
+                minMatrix[i + 1, j] = minMatrix[i, j] + matrix[i + 1, j];
+                Recalc(i + 1, j);
+            }
+
+
+
+        }
+
+
+
+        private int PathValue(int rad, int col, Direction direction)
+        {
+            int y = rad + (int)direction;
+            if (y == rows || y<0) return -1;
+
+            int path1 = minMatrix[y, col - 1] + matrix[y, col];
+            int pathValue = PathValue(y, col, direction);
+            if (pathValue < 0) return path1;
+            else
+            {
+                int path2 = pathValue + +matrix[y, col];
+                return path1 < path2 ? path1 : path2;
+            }
+        }       
+    }
+}
---
+++ jae.euler.test/level04/E083PathSumFourWaysUnitTest.cs
@@ -0,0 +1,69 @@
+﻿using jae.euler.lib;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E083PathSumFourWaysUnitTest
+    {
+     
+        private int[,] ReadMatrix()
+        {
+            int[,] matrix = new int[80,80];
+
+            int y = 0;
+            foreach (var line in File.ReadAllLines(@"DataFiles\p082_matrix.txt"))
+            {
+                int x = 0;
+                foreach(int number  in line.Split(',').Select(e => int.Parse(e)))
+                {  
+                    matrix[y, x++] = number;
+                }
+                y++;
+            }
+
+            return matrix;
+        }
+  
+
+        [Fact]
+        public void Test_1()
+        {
+            int[,] matrix =
+            {
+                {131,673,234,103,18 },
+                {201,96,342,965,150 },
+                {630,803,746,422,111},
+                {537,699,497,121,956},
+                {805,732,524,37,331}
+            };
+
+            var sut = new E083PathSumFourWays();
+            Assert.Equal(2297, sut.GetMinimalPathSum(matrix));
+        }
+
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+              Find the minimal path sum,
+              from the top left to the bottom right by only moving left, right, up, and down.
+            */
+
+            var matrix = ReadMatrix();
+
+            var sut = new E083PathSumFourWays();
+            Assert.Equal(425185, sut.GetMinimalPathSum(matrix));
+
+            /*
+              Congratulations, the answer you gave to problem 83 is correct.
+              You are the 15852nd person to have solved this problem.
+              This problem had a difficulty rating of 25%. 
+            */
+        }
+    }
+}
