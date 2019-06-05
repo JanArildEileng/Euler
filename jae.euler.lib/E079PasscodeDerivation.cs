bd3f47abf7538dfe69c64224edfc9d8c76647cec
janarild.eileng <janarild.eileng@fhi.no>
2019-06-05 07:48:31 +0200

    E079PasscodeDerivation

---
+++ jae.euler.lib/E079PasscodeDerivation.cs
@@ -0,0 +1,57 @@
+﻿using jae.euler.util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E079PasscodeDerivation
+    {
+        public int GetShortestPossiblePasscode(int[] table)
+        {
+            int shortestPossiblePasscode = 0;
+            HashSet<int>[] trailingNumbersSet = new HashSet<int>[10];
+            for (int i = 0; i < 10; i++)
+                trailingNumbersSet[i] = new HashSet<int>();
+
+            HashSet<int> foundDigits = new HashSet<int>();
+
+
+            for (int linenr = 0; linenr < table.Length; linenr++)
+            {
+                var digitsList = DigitsList.ConvertToDigitListe(table[linenr]);
+                trailingNumbersSet[digitsList[2]].Add(digitsList[1]);
+                trailingNumbersSet[digitsList[2]].Add(digitsList[0]);
+                trailingNumbersSet[digitsList[1]].Add(digitsList[0]);
+
+                foundDigits.Add(digitsList[0]);
+                foundDigits.Add(digitsList[1]);
+                foundDigits.Add(digitsList[2]);
+            }
+
+            var l = foundDigits.OrderBy(i => trailingNumbersSet[i].Count()).ToList();
+
+            shortestPossiblePasscode=DigitsList.ConvertToNumber(l);
+
+            return shortestPossiblePasscode;
+        }
+
+
+
+
+    }
+
+/*
+            319
+            680
+            180
+            690
+            129
+            620
+            762
+            689
+            762
+            318
+*/
+}
---
+++ jae.euler.test/level04/E079PasscodeDerivationUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using jae.euler.lib;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E079PasscodeDerivationUnitTest
+    {
+
+      
+        private int[] ReadTable()
+        {
+            int[] table = new int[50];
+
+            int linenr = 0;
+            foreach (var line in File.ReadAllLines(@"DataFiles\p079_keylog.txt"))
+            {
+                table[linenr++] = int.Parse(line);
+            }
+
+            return table;
+        }
+  
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+               determine the shortest possible secret passcode of unknown length.
+            */
+
+            var table = ReadTable();
+
+            var sut = new E079PasscodeDerivation();
+            Assert.Equal(73162890, sut.GetShortestPossiblePasscode(table));
+
+            /*
+                Congratulations, the answer you gave to problem 79 is correct.
+                You are the 37238th person to have solved this problem.
+                This problem had a difficulty rating of 5%. 
+            */
+        }
+    }
+}
