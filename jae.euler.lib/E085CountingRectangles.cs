fa73858966b83ea226890c9f01559d4491471e36
janarild.eileng <janarild.eileng@fhi.no>
2019-06-06 08:38:07 +0200

    E085CountingRectangles

---
+++ jae.euler.lib/E085CountingRectangles.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E085CountingRectangles
+    {
+        public long GetContainingRectangels(int width, int height)
+        {
+            long containingRectangels = 0;
+            for (int ystart=0;ystart< height;ystart++)
+            {
+                for (int yslutt = ystart+1; yslutt <= height ; yslutt++)
+                {
+                    for (int xstart = 0; xstart < width ; xstart++)
+                    {
+                        for (int xslutt = xstart + 1; xslutt <= width; xslutt++)
+                        {
+                            containingRectangels++;
+                        }
+                    }
+                }
+            }
+
+            return containingRectangels;
+        }
+
+        public int GetAreaNearestSolution()
+        {
+            long closestcontainingRectangelsDiff = long.MaxValue;
+            int areaNearestSolution = 0;
+
+
+            for (int width=1; width<100; width++)
+            {
+
+                for (int height = 1; height < 100; height++)
+                {
+                    long containingRectangels = GetContainingRectangels(width, height);
+
+                    long diff = Math.Abs(2000000 - containingRectangels);
+                    if (diff > closestcontainingRectangelsDiff) break;
+
+                    closestcontainingRectangelsDiff = diff;
+                    areaNearestSolution = height * width;
+                }
+            }
+
+            return areaNearestSolution;
+        }
+    }
+}
---
+++ jae.euler.test/level04/E085CountingRectanglesUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using jae.euler.lib;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E085CountingRectanglesUnitTest
+    {
+          
+        [Fact]
+        public void Test_1()
+        {
+            /*
+             it can be seen that a rectangular grid measuring 3 by 2 contains eighteen rectangles:
+            */
+            var sut = new E085CountingRectangles();
+            Assert.Equal(18, sut.GetContainingRectangels(3,2));
+        }
+
+        [Fact]
+        public void Test_2()
+        {
+            var sut = new E085CountingRectangles();
+            Assert.Equal(19936225, sut.GetContainingRectangels(94, 94));
+            Assert.Equal(20360400, sut.GetContainingRectangels(95, 94));       
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+              Although there exists no rectangular grid that contains exactly two million rectangles,
+              find the area of the grid with the nearest solution.
+            */
+
+
+            var sut = new E085CountingRectangles();
+            Assert.Equal(2772, sut.GetAreaNearestSolution());
+
+            /*
+              Congratulations, the answer you gave to problem 85 is correct.
+              You are the 21760th person to have solved this problem.
+              This problem had a difficulty rating of 15%. 
+            */
+        }
+    }
+}
