ede58116b07e9509ac1d93a55748913794ff894d
janarild.eileng <janarild.eileng@fhi.no>
2019-06-06 07:50:50 +0200

    E087PrimePowerTriples

---
+++ jae.euler.lib/E087PrimePowerTriples.cs
@@ -0,0 +1,41 @@
+﻿using jae.euler.math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E087PrimePowerTriples
+    {
+        public int GetCount(long below)
+        {
+            List<long> powerTriples = new List<long>();
+            List<long> primes = Primes.GetPrimeFactorsBelowNumber((long)Math.Sqrt(below));
+
+            foreach (long p1 in primes)
+            {
+                long squarePrime = p1 * p1;
+                long rest = below - squarePrime;
+
+                foreach (long p2 in primes)
+                {
+                    long triplePrime = p2 * p2 * p2;
+                    long rest2 = rest - triplePrime;
+                    if (rest2 <= 0) break;
+
+                    foreach (long p3 in primes)
+                    {
+                        long quadPrime = p3 * p3 * p3 * p3;
+                        long rest3 = rest2 - quadPrime;
+                        if (rest3 <= 0) break;
+
+                        powerTriples.Add(squarePrime + triplePrime + quadPrime);
+                    }
+                }
+            }
+
+            return powerTriples.Distinct().Count();
+
+        }
+    }
+}
---
+++ jae.euler.test/level04/E087PrimePowerTriplesUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using jae.euler.lib;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E087PrimePowerTriplesUnitTest
+    {
+
+        [Theory]
+        [InlineData(29, 1)]
+        [InlineData(34, 2)]
+        [InlineData(48, 3)]
+        [InlineData(50, 4)]
+        public void Test_1(int below, int expectedCount)
+        {
+            var sut = new E087PrimePowerTriples();
+            Assert.Equal(expectedCount, sut.GetCount(below));
+        }
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+               How many numbers below fifty million
+               can be expressed as the sum of a prime square, prime cube, and prime fourth power?
+            */
+
+            var sut = new E087PrimePowerTriples();
+            Assert.Equal(1097343, sut.GetCount(below: 50000000));
+
+            /*
+                Congratulations, the answer you gave to problem 87 is correct.
+                You are the 18246th person to have solved this problem.
+                This problem had a difficulty rating of 20%.
+            */
+        }
+    }
+}
