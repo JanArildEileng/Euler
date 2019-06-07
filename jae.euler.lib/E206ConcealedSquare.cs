7efc205838205e4771592fc3da5b7b24e4eb431a
janarild.eileng <janarild.eileng@fhi.no>
2019-06-07 08:22:45 +0200

    E206ConcealedSquare pre

---
+++ jae.euler.lib/E206ConcealedSquare.cs
@@ -0,0 +1,95 @@
+﻿using jae.euler.util;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E206ConcealedSquare
+    {
+
+        /*
+             Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
+              where each “_” is a single digit.
+          */
+
+        public long Square()
+        {
+            for (long i = 1010101010; i < 1010101030; i++)
+            {
+
+                BigInteger square=i*i;
+
+                var st = square.ToString();
+                if ( st.Length >19) throw new Exception($"to long square  {i}");
+                if (st.Length < 19) continue;
+
+                if (st[0]=='1' && st[2] == '2' && st[4] == '3' && st[6] == '4')
+                {
+                    if (st[8] == '5')
+                    {
+                        var a = 1;
+                    }
+                   
+                }
+
+
+            }
+
+            throw new Exception($"not found");
+
+        }
+
+
+
+            public long Square2()
+        {
+        
+
+
+
+                   
+            for (long i=1010101010;i< 1010101020; i++)
+            {
+                List<int> liste1 = DigitsList.ConvertToDigitListe(i);
+             
+
+                List<int> square = DigitsList.Product(liste1, liste1);
+
+                if (square.Count < 19) continue;
+                if (square.Count > 19) throw new Exception($"to long square  {i}");
+
+
+                square = DigitsList.ReverseCopy(square);
+
+                bool a = true;
+
+                for(int p=1;p<10;p++)
+                {
+                    //if (square[2 * p - 2] > p)
+                    //    throw new Exception($"to big {i} {p}  {square[2 * p - 2]}");
+
+
+                    if (square[2 *p -2 ] != p)
+                    {
+                        a = false;
+                        break;
+                    }
+                }
+
+                if (square[18] != 0)
+                {
+                    a = false;
+                 
+                }
+
+                if (a) return i;
+
+                
+            }
+
+            throw new Exception($"not found");
+        }
+    }
+}
---
+++ jae.euler.test/level09/E206ConcealedSquareUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using jae.euler.lib;
+using Xunit;
+
+namespace jae.euler.test.level09
+{
+    public class E206ConcealedSquareUnitTest
+    {
+
+        //[Theory]
+        //[InlineData(29, 1)]
+        //[InlineData(34, 2)]
+        //[InlineData(48, 3)]
+        //[InlineData(50, 4)]
+        //public void Test_1(int below, int expectedCount)
+        //{
+        //    var sut = new E206ConcealedSquare();
+        
+        //}
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+               Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
+                where each “_” is a single digit.
+            */
+
+            var sut = new E206ConcealedSquare();
+            Assert.Equal(-1, sut.Square());
+
+            /*
+             * 
+            */
+        }
+    }
+}
