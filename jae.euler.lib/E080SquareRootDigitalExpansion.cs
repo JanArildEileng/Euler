a1907db9f51624d357e7cda51542444bdb246ee3
janarild.eileng <janarild.eileng@fhi.no>
2019-06-07 16:18:49 +0200

    E080SquareRootDigitalExpansion pre

---
+++ jae.euler.lib/E080SquareRootDigitalExpansion.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using jae.euler.util;
+
+namespace jae.euler.lib
+{
+    public class E080SquareRootDigitalExpansion
+    {
+        public class PeriodParameters
+        {
+            public int an { get; set; }
+            public int sub { get; set; }
+            public double f { get; set; }
+        }
+
+
+
+        public int GetDigitalSumSquareRoot(int number)
+        {
+            //   GetPeriodsOfSquareRoot(number);
+            NumeratorHasMoreDigitsThanDenominator(number);
+            return 0;
+        }
+
+        public int GetDigitalSumSquareRoot()
+        {
+            int totalSum = 0;
+
+            for(int i=1;i<=100;i++)
+            {
+
+                int sum = GetDigitalSumSquareRoot(i);
+                totalSum += sum;
+            }
+
+            return totalSum;
+        }
+
+        public int GetPeriodsOfSquareRoot(int N)
+        {
+            int a0 = GetClosestIntegertToSquare(N);
+
+            if (a0 * a0 == N) return 0;
+
+            List<PeriodParameters> periodParameterList = new List<PeriodParameters>();
+
+            var periodParameters = new PeriodParameters { an = a0, sub = -a0, f = 1.0 };
+            periodParameterList.Add(periodParameters);
+
+            while (periodParameterList.Count < 300)
+            {
+                periodParameters = CalcNextPeriodParameters(a0, N, periodParameters);
+                periodParameterList.Add(periodParameters);
+
+                var treff = periodParameterList.Where(r => r.an == periodParameters.an && r.sub == periodParameters.sub && Math.Abs(r.f - periodParameters.f) < 0.10).ToList();
+
+                //if (treff.Count() == 2)
+                //{
+                //    int index = periodParameterList.IndexOf(treff[0]);
+                //    int index2 = periodParameterList.Count() - 1;
+
+                //    return index2 - index;
+                //}
+            }
+            throw new Exception($"Periode ikke funnet for {N}");
+        }
+
+        public PeriodParameters CalcNextPeriodParameters(int a0, int N, PeriodParameters tt)
+        {
+            int f2 = N - tt.sub * tt.sub;
+
+            int an = 0;
+            while (-((an + 1) * f2 / tt.f) + (-1) * tt.sub >= -a0)
+            {
+                an++;
+            }
+
+            int sub = (int)-((an) * f2 / tt.f) + (-1) * tt.sub;
+
+            var nextPeriodParameters = new PeriodParameters { an = an, sub = sub, f = (f2 / tt.f) };
+            return nextPeriodParameters;
+        }
+
+
+        private int GetClosestIntegertToSquare(int N)
+        {
+            int a0 = 1;
+
+            for (int i = 2; i < N; i++)
+            {
+                if (i * i <= N)
+                {
+                    a0 = i;
+                }
+                else
+                    break;
+            }
+            return a0;
+        }
+
+        public bool NumeratorHasMoreDigitsThanDenominator(int iterationNumber)
+        {
+            /*
+                 1 + 1/(2 + 1/2..)
+                 1+ 1/seriesum 
+
+              seriesum=t/n
+              seriesum=2 +1/seriesum = 2+1/(t/n)=2+n/t=(2*t+n)/t
+
+            */
+
+            List<int> teller = new List<int> { 2 };
+            List<int> nevner = new List<int> { 1 };
+
+            //seriesum=t/n = 2
+
+            while (iterationNumber > 1)
+            {
+                // (2*t+n)/t
+
+                //2*t
+                //       List<int> a = DigitsList.Product(teller, 2);
+
+                //2*t+n
+                var tmp2 = DigitsList.Sum(DigitsList.Product(teller, 2), nevner);
+                // /t
+                nevner = teller;
+                teller = tmp2;
+
+                iterationNumber--;
+            }
+
+            //1 + 1 / seriesum
+            // 1 +1/(t/n) = (n+t)/t
+            var tmp = teller;
+            teller = DigitsList.Sum(nevner, teller);
+            nevner = tmp;
+
+            //finn all siffer i teller og nevner
+            return (teller.Count >100);
+        }
+
+
+    }
+}
---
+++ jae.euler.test/level04/E080SquareRootDigitalExpansionUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using jae.euler.lib;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E080SquareRootDigitalExpansionUnitTest
+    {
+
+      
+       
+
+        [Fact]
+        public void Test_1()
+        {
+            /*
+             * The square root of two is 1.41421356237309504880...,
+             * and the digital sum of the first one hundred decimal digits is 475.
+             */
+
+
+              var sut = new E080SquareRootDigitalExpansion();
+              Assert.Equal(475, sut.GetDigitalSumSquareRoot(2));
+          }
+
+
+
+          [Fact]
+          public void Solution()
+          {
+              /*For the first one hundred natural numbers,
+               * find the total of the digital sums of the first one hundred decimal digits 
+               * for all the irrational square roots.
+
+              */
+
+
+
+
+            var sut = new E080SquareRootDigitalExpansion();
+            Assert.Equal(-1, sut.GetDigitalSumSquareRoot());
+
+            /*
+                
+            */
+        }
+    }
+}
