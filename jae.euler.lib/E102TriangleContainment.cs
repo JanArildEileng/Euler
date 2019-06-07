a1a0cdc9daa8cb4ae0d5d1982af54d375084577e
janarild.eileng <janarild.eileng@fhi.no>
2019-06-07 15:38:47 +0200

    E102TriangleContainment

---
+++ jae.euler.lib/E102TriangleContainment.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E102TriangleContainment
+    {
+        public bool ContaingOrigion(string coordinatString)
+        {
+            var coordinates = coordinatString.Split(',').Select(e => double.Parse(e)).ToList();
+
+            var x = new List<double>
+            {
+                coordinates[0],coordinates[2],coordinates[4]
+            };
+
+            var y = new List<double>
+            {
+                coordinates[1],coordinates[3],coordinates[5]
+            };
+
+
+            if (!(x.Max() > 0 && x.Min() < 0 && y.Max() > 0 && y.Min() < 0))
+                return false;
+            //finn først det ene hjørenet som ligger alene på høyre eller venstre siden av y-aksen
+            //finn så skjæringspunketene ( y-aksen) for linjene til de andre punktene, 
+            //dersom disse skjæringspunkene har ulike fortegn, er origin i trianglet...
+
+            (double bx1, double bx2) yCross=(0,0);
+            if (  (x[0] >0 & x[1] <0 && x[2]<0) || (x[0] < 0 & x[1] > 0 && x[2] > 0))
+            {
+                yCross = CalcYCross(x, y);
+            };
+
+            if ( (x[1] > 0 & x[0] < 0 && x[2] < 0) || (x[1] < 0 & x[0] > 0 && x[2] > 0))
+            {
+                yCross = CalcYCross(new List<double> { x[1],x[0],x[2]} , new List<double> { y[1], y[0], y[2] });
+            };
+
+            if ( (x[2] > 0 & x[0] < 0 && x[1] < 0) || (x[2] < 0 & x[0] > 0 && x[1] > 0))
+            {
+               yCross = CalcYCross(new List<double> { x[2], x[1], x[0] }, new List<double> { y[2], y[1], y[0] });
+            };
+
+            return (yCross.bx1 * yCross.bx2 <= 0) ? true : false;
+        }
+
+        public int GetNumberOfTrianglesContaingOrigion(string[] coordinateList)
+        {
+            int numberOfTrianglesContaingOrigion = 0;
+            foreach (var coordinate in coordinateList) {
+                if  ( ContaingOrigion(coordinate))
+                {
+                    numberOfTrianglesContaingOrigion++;
+                }
+            }
+
+            return numberOfTrianglesContaingOrigion;
+        }
+
+        private (double bx1,double bx2) CalcYCross(List<double> x, List<double> y )
+        {
+             double[] ax1 =
+             {
+                     (y[1]-y[0])/(x[1]-x[0]),
+                     (y[2]-y[0])/(x[2]-x[0])
+             };
+
+            return (y[0] - ax1[0] * x[0], y[0] - ax1[1] * x[0]);
+        }
+
+
+    }
+}
---
+++ jae.euler.test/level05/E102TriangleContainmentUnitTest.cs
@@ -0,0 +1,89 @@
+﻿using jae.euler.lib;
+using System.IO;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace jae.euler.test.level05
+{
+    public class E102TriangleContainmentUnitTest
+    {
+
+
+        private string[] ReadFile()
+        {
+            // -340,495,-153,-910,835,-947
+
+
+            string[] coordinates = File.ReadAllLines(@"DataFiles\p102_triangles.txt");
+            return coordinates;
+        }
+
+
+        [Theory]
+        [InlineData("10,10,10,0,-10,-10")]
+        public void Test_ContaingOrigion(string coordinates)
+        {
+
+            var sut = new E102TriangleContainment();
+            Assert.True(sut.ContaingOrigion(coordinates));
+        }
+
+        [Theory]
+  //      [InlineData("10,10,10,0,0,10")]
+        [InlineData("10,0,8,10,-1,-5")]
+        public void Test_NotContaingOrigion(string coordinates)
+        {
+
+            var sut = new E102TriangleContainment();
+            Assert.False(sut.ContaingOrigion(coordinates));
+        }
+
+
+
+
+
+        [Fact]
+        public void Test_ABC()
+        {
+
+            string[] coordinates = ReadFile();
+            Assert.Equal(1000, coordinates.Length);
+            var sut = new E102TriangleContainment();
+            Assert.True(sut.ContaingOrigion(coordinates[0]));
+        }
+
+        [Fact]
+        public void Test_XYZ()
+        {
+
+            string[] coordinates = ReadFile();
+            var sut = new E102TriangleContainment();
+            Assert.False(sut.ContaingOrigion(coordinates[1]));
+        }
+
+
+
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+                find the number of triangles for which the interior contains the origin.
+               
+            */
+            string[] coordinates = ReadFile();
+
+
+            var sut = new E102TriangleContainment();
+            Assert.Equal(228, sut.GetNumberOfTrianglesContaingOrigion(coordinates));
+
+            /*
+              Congratulations, the answer you gave to problem 102 is correct.
+              You are the 19200th person to have solved this problem.
+              This problem had a difficulty rating of 15%
+             
+            */
+        }
+    }
+}
