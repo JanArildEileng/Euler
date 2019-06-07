e81a157a8558db360c34112724786088ca4686f3
janarild.eileng <janarild.eileng@fhi.no>
2019-06-07 11:56:51 +0200

    E112BouncyNumbers

---
+++ jae.euler.lib/E112BouncyNumbers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jae.euler.lib
+{
+    public class E112BouncyNumbers
+    {
+        public int GetLeastNumber(int proportion)
+        {
+            int total = 0;
+            int bouncy = 0;
+            int current = 0;
+
+            while(true)
+            {
+                current++;
+                total++;
+                if (IsBounching(current))
+                {
+                    bouncy++;
+                }
+
+                if (total * proportion == 100* bouncy)
+                    return current;
+            }
+        }
+
+        public bool IsBounching(int number)
+        {
+            char[] chars = number.ToString().ToCharArray();
+
+            for (int i = 1; i < chars.Length; i++) { 
+                if (chars[i - 1] < chars[i]) {
+                    for (int j = 1; j < chars.Length; j++)
+                        if (chars[j - 1] > chars[j])
+                            return true;
+
+                }
+            }
+
+            return false;
+        }
+
+
+
+    }
+}
---
+++ jae.euler.test/level05/E112BouncyNumbersUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using jae.euler.lib;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace jae.euler.test.level05
+{
+    public class E112BouncyNumbersUnitTest
+    {
+
+        [Theory]
+        [InlineData(123456789,false)]
+        [InlineData(98765431, false)]
+        [InlineData(152852, true)]
+        public void Test_IsBounching(int number,bool bounching)
+        {
+            var sut = new E112BouncyNumbers();
+            Assert.Equal(bounching, sut.IsBounching(number));
+        }
+
+
+
+
+        [Fact]
+        public void Test_Proportion_50()
+        {
+            var sut = new E112BouncyNumbers();
+            Assert.Equal(538, sut.GetLeastNumber(proportion: 50));
+        }
+
+        [Fact]
+        public void Test_Proportion_90()
+        {
+            var sut = new E112BouncyNumbers();
+            Assert.Equal(21780, sut.GetLeastNumber(proportion: 90));
+        }
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+             * Find the least number for which the proportion of bouncy numbers is exactly 99%.
+               
+            */
+
+            var sut = new E112BouncyNumbers();
+            Assert.Equal(1587000, sut.GetLeastNumber(proportion:99));
+
+            /*
+              Congratulations, the answer you gave to problem 112 is correct.
+              You are the 21723rd person to have solved this problem.
+              This problem had a difficulty rating of 15%.
+             
+            */
+        }
+    }
+}
