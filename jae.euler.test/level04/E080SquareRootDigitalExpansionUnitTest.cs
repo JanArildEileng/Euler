7dff3ebbe1b1b30a5e387c1a16fa93b5c6399797
jan eileng <jaeileng@gmail.com>
2019-06-08 20:51:52 +0200

    E080SquareRootDigitalExpansion

--- jae.euler.test/level04/E080SquareRootDigitalExpansionUnitTest.cs
+++ jae.euler.test/level04/E080SquareRootDigitalExpansionUnitTest.cs
@@ -8,9 +8,9 @@ namespace jae.euler.test.level04
 {
     public class E080SquareRootDigitalExpansionUnitTest
     {
+        //løst vha:
+        // https://www.mathblog.dk/project-euler-80-digits-irrational-square-roots/
 
-      
-       
 
         [Fact]
         public void Test_1()
@@ -19,7 +19,6 @@ namespace jae.euler.test.level04
              * The square root of two is 1.41421356237309504880...,
              * and the digital sum of the first one hundred decimal digits is 475.
              */
-
 
               var sut = new E080SquareRootDigitalExpansion();
               Assert.Equal(475, sut.GetDigitalSumSquareRoot(2));
@@ -33,17 +32,15 @@ namespace jae.euler.test.level04
               /*For the first one hundred natural numbers,
                * find the total of the digital sums of the first one hundred decimal digits 
                * for all the irrational square roots.
-
               */
 
-
-
-
             var sut = new E080SquareRootDigitalExpansion();
-            Assert.Equal(-1, sut.GetDigitalSumSquareRoot());
+            Assert.Equal(40886, sut.GetDigitalSumSquareRootAll());
 
             /*
-                
+                Congratulations, the answer you gave to problem 80 is correct.
+                You are the 17365th person to have solved this problem.
+                This problem had a difficulty rating of 20%. 
             */
         }
     }
