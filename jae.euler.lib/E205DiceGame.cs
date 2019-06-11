758ff2e7ec31e570f9a0b99f3d6c37e4b2eb0464
janarild.eileng <janarild.eileng@fhi.no>
2019-06-11 08:27:54 +0200

    E205DiceGame

---
+++ jae.euler.lib/E205DiceGame.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E205DiceGame
+    {
+        /*        Peter has nine four-sided(pyramidal) dice, each with faces numbered 1,  2, 3, 4.
+        Colin has six six-sided(cubic) dice, each with faces numbered 1, 2, 3, 4, 5, 6.
+        */
+
+        double[] pyramidalProbablitity;
+        double[] cubicProbablitity;
+
+        public E205DiceGame()
+        {
+            pyramidalProbablitity= calcProbablity(9,4);
+            cubicProbablitity = calcProbablity(6, 6);
+        }
+
+        public string GetProbablitityPyramidalBeatsCubic()
+        {
+            double total = 0.0;
+
+            for(int i=1;i<=36;i++)
+            {
+                double pyramidalP = pyramidalProbablitity[i];
+                double sumCubicProbablitity = cubicProbablitity.Take(i).Sum();
+                total += sumCubicProbablitity * pyramidalP;
+            }
+
+            return $"{total:0.0000000}";
+        }
+
+
+        private double[] calcProbablity(int diceNummbers, int maxValue)
+        {
+            int size= (diceNummbers * maxValue) + 1;
+            int[] counts = new int[size];
+
+            Action<int> increaseCount = (value) =>
+              {
+                  counts[value]++;
+              };
+
+            ThrowDice(diceNummbers, maxValue, 0, increaseCount);
+            var sum = counts.Sum(); ;
+
+            var probablitity = counts.Select(e => 1.0 * e / sum).ToArray();
+            return probablitity;
+        }
+
+
+        private void ThrowDice(int diceNummber,int maxValue,int sumValue,Action<int> action)
+        {
+            if (diceNummber == 0)
+            {
+                action(sumValue);
+                return;
+            }
+            for (int value = 1; value <= maxValue; value++)
+            {
+                ThrowDice(diceNummber - 1, maxValue, sumValue+value,action);
+            }
+        }
+
+        
+    }
+}
---
+++ jae.euler.test/level09/E205DiceGameUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using jae.euler.lib;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace jae.euler.test.level09
+{
+    public class E205DiceGameUnitTest
+    {
+        [Fact]
+        public void Test_1()
+        {
+            var sut = new E205DiceGame();
+        }
+     
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+               What is the probability that Pyramidal Pete beats Cubic Colin? 
+               Give your answer rounded to seven decimal places in the form 0.abcdefg
+            */
+
+            var sut = new E205DiceGame();
+            Assert.Equal("0,5731441", sut.GetProbablitityPyramidalBeatsCubic());
+
+            /*
+             Congratulations, the answer you gave to problem 205 is correct.
+             You are the 13152nd person to have solved this problem.
+             This problem had a difficulty rating of 15%.
+
+            */
+        }
+    }
+}
