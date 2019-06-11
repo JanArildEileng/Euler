abe1b53fab82ddd44e499c580321630eb81655a6
janarild.eileng <janarild.eileng@fhi.no>
2019-06-11 09:46:45 +0200

    E096SuDoku pre

---
+++ jae.euler.lib/E096SuDoku.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+
+    public class E096SuDoku
+    {
+        public int GetTopLeftCorner(int[][] sudoku)
+        {
+
+            while (solveStep1(sudoku) >0)
+            {
+
+            }
+          
+
+            return (100*sudoku[0][0]+ 10*sudoku[0][1]+ sudoku[0][2]);
+        }
+
+        public int GetSumTopLeftCorner(List<int[][]> sudokus)
+        {
+            int sumTopLeftCorner = 0;
+
+            foreach(var sudoku in sudokus)
+            {
+                sumTopLeftCorner += GetTopLeftCorner(sudoku);
+            }
+
+            return sumTopLeftCorner;
+        }
+
+
+
+
+        private int solveStep1(int[][] sudoku)
+        {
+            HashSet<int>[,] set = new HashSet<int>[9,9]; 
+            for(int i=0;i<9;i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (sudoku[i][j] == 0)
+                        set[i, j] = new HashSet<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                }
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (set[i, j]  !=null)
+                    {
+                        set[i, j] = set[i, j].Except(GetUsedRow(sudoku, i)).ToHashSet();
+                        set[i, j] = set[i, j].Except(GetUsedCol(sudoku, j)).ToHashSet();
+                        set[i, j] = set[i, j].Except(GetUseBox(sudoku,i, j)).ToHashSet();
+                
+                    }
+                      
+                }
+            }
+
+            int count=0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (sudoku[i][j] == 0 && set[i, j].Count==1)
+                    {
+                        sudoku[i][j] = set[i, j].First();
+                        count++;
+                    }
+                       
+                }
+            }
+
+           
+            if (count==0)
+            {
+
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (sudoku[i][j] == 0)
+                        {
+
+                            var a = 1;
+                        }
+
+                    }
+                }
+
+
+            }
+
+                
+           return count;
+        }
+
+        private List<int> GetUsedRow(int[][] sudoku,int row)
+        {
+            return sudoku[row].Where(e => e != 0).ToList();
+
+        }
+        private List<int> GetUsedCol(int[][] sudoku, int col)
+        {
+            List<int> list = new List<int>();
+            for (int row=0;row<9;row++)
+            {
+                if (sudoku[row][col] != 0)
+                    list.Add(sudoku[row][col]);
+            }
+
+            return list;
+        }
+
+        private List<int> GetUseBox(int[][] sudoku,int row, int col)
+        {
+            List<int> list = new List<int>();
+
+            int rowstart = (row / 3) * 3;
+            int rowend = rowstart + 3;
+            int colstart = (col / 3) * 3;
+            int colend = colstart + 3;
+
+
+            for (int i = rowstart; i < rowend; i++)
+            {
+
+                for (int j = colstart; j < colend; j++)
+                {
+                    if (sudoku[i][j] != 0)
+                        list.Add(sudoku[i][j]);
+                }
+
+            }
+
+            return list;
+        }
+
+
+    }
+}
---
+++ jae.euler.test/level04/E096SuDokuUnitTest.cs
@@ -0,0 +1,79 @@
+﻿using jae.euler.lib;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace jae.euler.test.level04
+{
+    public class E096SuDokuUnitTest
+    {
+/*
+        Grid 01
+003020600
+900305001
+001806400
+008102900
+700000008
+006708200
+002609500
+800203009
+005010300
+*/
+
+        private List<int[][]> GetSudokusFromFile()
+        {
+            List<int[][]> sudokoListe = new List<int[][]>();
+            var file= File.OpenText("DataFiles\\p096_sudoku.txt");
+            for (int grid=1;grid<=50;grid++)
+            {
+                file.ReadLine();
+                int[][] sudoko = new int[9][];
+
+                for (int i=0;i<9;i++)
+                {
+                   var line=file.ReadLine();
+                    sudoko[i] = line.ToCharArray().Select(c => c - '0').ToArray<int>();
+                }
+
+                sudokoListe.Add(sudoko);
+            }
+
+            return sudokoListe;
+        }
+
+
+        [Fact]
+        public void Test_First()
+        {
+            /*
+                483 is the 3-digit number found in the top left corner of the solution grid above.
+            */
+
+            var sudokus = GetSudokusFromFile();
+            var sut = new E096SuDoku();
+            Assert.Equal(483, sut.GetTopLeftCorner(sudokus[0]));
+
+        }
+
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+              By solving all fifty puzzles find the sum of the 3-digit numbers found in the top left corner of each solution grid; for example, 
+              
+            483 is the 3-digit number found in the top left corner of the solution grid above.
+            */
+            var sudokus = GetSudokusFromFile();
+            var sut = new E096SuDoku();
+            Assert.Equal(-1, sut.GetSumTopLeftCorner(sudokus));
+
+
+            /*
+                
+            */
+        }
+    }
+}
