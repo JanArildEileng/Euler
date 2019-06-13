2b4011fb93eafa7379009932165c9ad1f4692c48
janarild.eileng <janarild.eileng@fhi.no>
2019-06-13 16:19:46 +0200

    E357PrimeGeneratingIntegers pre

---
+++ jae.euler.lib/E357PrimeGeneratingIntegers.cs
@@ -0,0 +1,49 @@
+﻿using jae.euler.math;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace jae.euler.lib
+{
+    public class E357PrimeGeneratingIntegers
+    {
+        public int Max { get; set; }
+        Dictionary<long,long> primeDictionary;
+
+        public E357PrimeGeneratingIntegers(int Max)
+        {
+            this.Max = Max;
+
+            primeDictionary = Primes.GetPrimeFactorsBelowNumber((long)(Max+2)).ToDictionary(e=>e);
+
+        }
+
+
+        /*
+          for every divisor d of n, d+n/d is prime.
+         */    
+        public bool IsPrimeGenerating(long number)
+        {
+            List<long> divisors = Divisors.GetAllUniqueDivisorsIn(number);
+            List<long> liste = divisors.Select(d => d + number / d).Distinct().ToList();
+
+
+            return liste.All(d => primeDictionary.ContainsKey(d));
+        }
+
+        public long GetSumOfAllPositiveIntegers()
+        {
+            long sumOfAllPositiveIntegers = 0;
+
+            for (long i=1;i< Max;i++)
+            {
+                if (IsPrimeGenerating(i))
+                    sumOfAllPositiveIntegers += i;
+
+            }
+
+            return sumOfAllPositiveIntegers;
+        }
+    }
+}
---
+++ jae.euler.test/level15/E357PrimeGeneratingIntegersUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using jae.euler.lib;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace jae.euler.test.level15
+{
+    public class E357PrimeGeneratingIntegersUnitTest
+    {
+        [Fact]
+        public void Test_30IsPrimeGenerating()
+        {
+            var sut = new E357PrimeGeneratingIntegers(Max: 30);
+            Assert.True(sut.IsPrimeGenerating(30));
+
+        }
+
+        [Fact]
+        public void Test_30Max()
+        {
+            var sut = new E357PrimeGeneratingIntegers(Max: 30);
+            Assert.Equal(30, sut.GetSumOfAllPositiveIntegers());
+
+        }
+
+
+
+        [Fact]
+        public void Solution()
+        {
+            /*
+              Find the sum of all positive integers n not exceeding 100 000 000
+                such that for every divisor d of n, d+n/d is prime.
+            */
+
+            var sut = new E357PrimeGeneratingIntegers(Max: 1000
+                00000);
+     //       Assert.Equal(-1, sut.GetSumOfAllPositiveIntegers());
+
+            /*
+           
+
+            */
+        }
+    }
+}
