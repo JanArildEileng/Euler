9ca8d9bda4ad646791cfd2506b9d779708c44beb
janarild.eileng <janarild.eileng@fhi.no>
2020-01-16 10:09:29 +0100

    using Enumerable .Range

--- jae.euler.lib/E001-E100/E001Multiplesof3and5.cs
+++ jae.euler.lib/E001-E100/E001Multiplesof3and5.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace jae.euler.lib
 {
@@ -6,11 +8,11 @@ namespace jae.euler.lib
     {
         public int Sum(int below)
         {
-            return
-                Enumerable.
-                Range(1, below-1).
-                Where(i => (i % 3 == 0) || (i % 5 == 0)).
-                Sum();
+
+           return  Enumerable
+                .Range(1, count:below - 1)
+                .Where(i => (i % 3 == 0) || (i % 5) == 0)
+                .Sum();
         }
     }
 }
